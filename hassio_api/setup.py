d12ab70f907c2b9cc5f9f0f8309573389e5f4d0e
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-03-18 19:02:19 +0100

    Add struct for python api

---
+++ hassio_api/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+VERSION = "1.5"
+
+setup(
+    name='HassIO',
+    version=VERSION,
+    license='BSD License',
+    author='The Home Assistant Authors',
+    author_email='hello@home-assistant.io',
+    url='https://home-assistant.io/',
+    description=('Open-source private cloud os for Home-Assistant'
+                 ' based on ResinOS'),
+    long_description=('A maintenainless private cloud operator system that'
+                      'setup a Home-Assistant instance. Based on ResinOS'),
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Home Automation'
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords=['docker', 'home-assistant', 'api'],
+    zip_safe=False,
+    platforms='any',
+    packages=['hassio'],
+    include_package_data=True,
+    install_requires=[
+        'async_timeout',
+        'aiohttp',
+        'docker-py'
+    ]
+)
