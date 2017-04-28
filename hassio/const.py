dadb72aca8449de83df2996bd8450852644022f9
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-04-28 02:09:21 +0200

    Pump version 0.19

--- hassio/const.py
+++ hassio/const.py
@@ -1,5 +1,5 @@
 """Const file for HassIO."""
-HASSIO_VERSION = '0.18'
+HASSIO_VERSION = '0.19'
 
 URL_HASSIO_VERSION = ('https://raw.githubusercontent.com/home-assistant/'
                       'hassio/master/version.json')
