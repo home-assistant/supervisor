dbe6b860c71fbde796ec5b886a91cc2d2e45995e
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-05-02 02:28:40 +0200

    Pump version

--- hassio/const.py
+++ hassio/const.py
@@ -1,7 +1,7 @@
 """Const file for HassIO."""
 from pathlib import Path
 
-HASSIO_VERSION = '0.19'
+HASSIO_VERSION = '0.20'
 
 URL_HASSIO_VERSION = ('https://raw.githubusercontent.com/home-assistant/'
                       'hassio/master/version.json')
