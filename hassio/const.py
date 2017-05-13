eed41d30ec68a8c2303e54afbd1db7de0e15710a
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-05-13 23:17:23 +0200

    Update const.py

--- hassio/const.py
+++ hassio/const.py
@@ -1,7 +1,7 @@
 """Const file for HassIO."""
 from pathlib import Path
 
-HASSIO_VERSION = '0.25'
+HASSIO_VERSION = '0.26'
 
 URL_HASSIO_VERSION = ('https://raw.githubusercontent.com/home-assistant/'
                       'hassio/master/version.json')
