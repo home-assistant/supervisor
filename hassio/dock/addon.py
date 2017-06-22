f9d21cfdabfe99e0813f22425d12d57e0473b3f1
pvizeli <pvizeli@syshack.ch>
2017-06-22 18:25:23 +0200

    fix bug p4

--- hassio/dock/addon.py
+++ hassio/dock/addon.py
@@ -145,7 +145,7 @@ class DockerAddon(DockerBase):
 
         Need run inside executor.
         """
-        build_dir = Path(self.config.path_addons_build, self.addon)
+        build_dir = Path(self.config.path_addons_build, self.addon.slug)
         try:
             # prepare temporary addon build folder
             try:
