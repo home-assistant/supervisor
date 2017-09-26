17951030866f4861a284833a55e7aed93c77aa4d
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-09-26 09:10:56 +0200

    Pump version to 0.65

--- hassio/const.py
+++ hassio/const.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 from ipaddress import ip_network
 
-HASSIO_VERSION = '0.64'
+HASSIO_VERSION = '0.65'
 
 URL_HASSIO_VERSION = ('https://raw.githubusercontent.com/home-assistant/'
                       'hassio/{}/version.json')
