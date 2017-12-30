169520e3fa0b7fc96762113ef5055d58893e95e2
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-12-30 18:58:28 +0100

    Update core

--- hassio/core.py
+++ hassio/core.py
@@ -54,71 +54,71 @@ class HassIO(CoreSysAttributes):
             now=True)
 
         # Load homeassistant
-        await self.homeassistant.prepare()
+        await self._homeassistant.prepare()
 
         # Load addons
-        await self.addons.prepare()
+        await self._addons.prepare()
 
         # schedule addon update task
-        self.scheduler.register_task(
-            self.addons.reload, RUN_RELOAD_ADDONS_TASKS, now=True)
-        self.scheduler.register_task(
-            addons_update(self.loop, self.addons), RUN_UPDATE_ADDONS_TASKS)
+        self._scheduler.register_task(
+            self._addons.reload, RUN_RELOAD_ADDONS_TASKS, now=True)
+        self._scheduler.register_task(
+            addons_update(self._loop, self._addons), RUN_UPDATE_ADDONS_TASKS)
 
         # schedule self update task
-        self.scheduler.register_task(
-            hassio_update(self.supervisor, self.updater),
+        self._scheduler.register_task(
+            hassio_update(self._supervisor, self._updater),
             RUN_UPDATE_SUPERVISOR_TASKS)
 
         # schedule snapshot update tasks
-        self.scheduler.register_task(
-            self.snapshots.reload, RUN_RELOAD_SNAPSHOTS_TASKS, now=True)
+        self._scheduler.register_task(
+            self._snapshots.reload, RUN_RELOAD_SNAPSHOTS_TASKS, now=True)
 
         # start dns forwarding
-        self.loop.create_task(self.dns.start())
+        self._loop.create_task(self._dns.start())
 
         # start addon mark as initialize
-        await self.addons.auto_boot(STARTUP_INITIALIZE)
+        await self._addons.auto_boot(STARTUP_INITIALIZE)
 
     async def start(self):
         """Start HassIO orchestration."""
         # on release channel, try update itself
         # on beta channel, only read new versions
         await asyncio.wait(
-            [hassio_update(self.supervisor, self.updater)()],
+            [hassio_update(self._supervisor, self._updater)()],
             loop=self.loop
         )
 
         # start api
-        await self.api.start()
-        _LOGGER.info("Start hassio api on %s", self.docker.network.supervisor)
+        await self._api.start()
+        _LOGGER.info("Start hassio api on %s", self._docker.network.supervisor)
 
         try:
             # HomeAssistant is already running / supervisor have only reboot
-            if self.hardware.last_boot == self.config.last_boot:
+            if self._hardware.last_boot == self._config.last_boot:
                 _LOGGER.info("HassIO reboot detected")
                 return
 
             # start addon mark as system
-            await self.addons.auto_boot(STARTUP_SYSTEM)
+            await self._addons.auto_boot(STARTUP_SYSTEM)
 
             # start addon mark as services
-            await self.addons.auto_boot(STARTUP_SERVICES)
+            await self._addons.auto_boot(STARTUP_SERVICES)
 
             # run HomeAssistant
-            if self.homeassistant.boot:
-                await self.homeassistant.run()
+            if self._homeassistant.boot:
+                await self._homeassistant.run()
 
             # start addon mark as application
-            await self.addons.auto_boot(STARTUP_APPLICATION)
+            await self._addons.auto_boot(STARTUP_APPLICATION)
 
             # store new last boot
-            self.config.last_boot = self.hardware.last_boot
+            self._config.last_boot = self._hardware.last_boot
 
         finally:
             # schedule homeassistant watchdog
-            self.scheduler.register_task(
-                homeassistant_watchdog_docker(self.loop, self.homeassistant),
+            self._scheduler.register_task(
+                homeassistant_watchdog_docker(self._loop, self._homeassistant),
                 RUN_WATCHDOG_HOMEASSISTANT_DOCKER)
 
             # self.scheduler.register_task(
@@ -126,17 +126,18 @@ class HassIO(CoreSysAttributes):
             #    RUN_WATCHDOG_HOMEASSISTANT_API)
 
             # If landingpage / run upgrade in background
-            if self.homeassistant.version == 'landingpage':
-                self.loop.create_task(self.homeassistant.install())
+            if self._homeassistant.version == 'landingpage':
+                self._loop.create_task(self._homeassistant.install())
 
     async def stop(self):
         """Stop a running orchestration."""
         # don't process scheduler anymore
-        self.scheduler.suspend = True
+        self._scheduler.suspend = True
 
         # process stop tasks
-        self.websession.close()
-        self.homeassistant.websession.close()
+        self._websession.close()
+        self._websession_ssl.close()
 
         # process async stop tasks
-        await asyncio.wait([self.api.stop(), self.dns.stop()], loop=self.loop)
+        await asyncio.wait(
+            [self._api.stop(), self._dns.stop()], loop=self._loop)
