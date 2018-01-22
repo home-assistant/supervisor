c17df6215a4c09b9bcfdde6124238b821044fe1c
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-01-22 21:41:35 +0100

    Update __init__.py

--- hassio/api/__init__.py
+++ hassio/api/__init__.py
@@ -172,6 +172,8 @@ class RestAPI(CoreSysAttributes):
         # This route is for backwards compatibility with HA < 0.58
         self.webapp.router.add_get(
             '/panel', create_panel_response('hassio-main-es5'))
+        
+        # This route is for backwards compatibility with HA 0.58 - 0.61
         self.webapp.router.add_get(
             '/panel_es5', create_panel_response('hassio-main-es5'))
         self.webapp.router.add_get(
