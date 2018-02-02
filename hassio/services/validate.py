0afa4d4ecc646f3842302c5c209f01bbbc381211
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-02-02 14:36:29 +0100

    Update validate.py

--- hassio/services/validate.py
+++ hassio/services/validate.py
@@ -8,23 +8,24 @@ from ..const import (
 from ..validate import NETWORK_PORT
 
 
-SCHEMA_SERVICE = vol.Schema({
-    vol.Optional(ATTR_PROVIDER): vol.Coerce(str),
+# pylint: disable=no-value-for-parameter
+SCHEMA_SERVICE_MQTT = vol.Schema({
+    vol.Required(ATTR_HOST): vol.Coerce(str),
+    vol.Required(ATTR_PORT): NETWORK_PORT,
+    vol.Optional(ATTR_USERNAME): vol.Coerce(str),
+    vol.Optional(ATTR_PASSWORD): vol.Coerce(str),
+    vol.Optional(ATTR_SSL, default=False): vol.Boolean(),
+    vol.Optional(ATTR_PROTOCOL, default='3.1.1'):
+        vol.All(vol.Coerce(str), vol.In(['3.1', '3.1.1'])),
 })
 
 
-# pylint: disable=no-value-for-parameter
-SCHEMA_SERVICE_MQTT = SCHEMA_SERVICE.extend({
-    vol.Optional(ATTR_HOST): vol.Coerce(str),
-    vol.Optional(ATTR_PORT): NETWORK_PORT,
-    vol.Optional(ATTR_USERNAME): vol.Coerce(str),
-    vol.Optional(ATTR_PASSWORD): vol.Coerce(str),
-    vol.Optional(ATTR_SSL): vol.Boolean(),
-    vol.Optional(ATTR_PROTOCOL):
-        vol.All(vol.Coerce(str), vol.In(['3.1', '3.1.1'])),
-}, extra=vol.REMOVE_EXTRA)
+SCHEMA_CONFIG_MQTT = SCHEMA_SERVICE_MQTT.extend({
+    vol.Required(ATTR_PROVIDER): vol.Coerce(str),
+})
 
 
 SCHEMA_SERVICES_FILE = vol.Schema({
-    vol.Optional(ATTR_MQTT, default=dict): SCHEMA_SERVICE_MQTT,
+    vol.Optional(ATTR_MQTT, default=dict): 
+        vol.Any({}, SCHEMA_CONFIG_MQTT),
 }, extra=vol.REMOVE_EXTRA)
