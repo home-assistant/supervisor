0d46e8fff000a99afa1b445762f03d8969d95292
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-02-12 15:42:13 +0100

    Update tar.py

--- hassio/utils/tar.py
+++ hassio/utils/tar.py
@@ -1,4 +1,5 @@
 """Tarfile fileobject handler for encrypted files."""
+import tarfile
 
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
@@ -13,18 +14,42 @@ MOD_WRITE = 'w'
 class SecureTarFile(object):
     """Handle encrypted files for tarfile library."""
 
-    def __init__(self, name, mode, key=None, gzip=True):
+    def __init__(self, name, mode, key, gzip=True):
         """Initialize encryption handler."""
-        tar_mode = f"{mode}|gz" if gzip else f"{mode}|"
-        
+        assert len(key) % 16 in (16, 24, 32)
+
+        # File
+        self._file = None
+        self._mode = mode
+        self._name = name
+
+        # Tarfile options
+        self._tar = None
+        self._tar_mode = f"{mode}|gz" if gzip else f"{mode}|"
+
+        # Encryption/Decription
+        self._aes = None
+        self._key = key
+
+    def __enter__(self):
+        """Start context manager tarfile."""
+        self._file = self._name.open(self._mode)
+
         # Extract IV for CBC
-        if mode == MOD_READ:
+        if self._mode == MOD_READ:
             cbc_iv = self._file.read(16)
         else:
             cbc_iv = get_random_bytes(16)
             self._file.write(cbc_iv)
+        self._aes = AES.new(self._key, AES.MODE_CBC, iv=cbc_iv)
 
-        self._aes = AES.new(key, AES.MODE_CBC, iv=cbc_iv)
+        self._tar = tarfile.open(fileobj=self, self._tar_mode)
+        return self._tar
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Close file."""
+        self._tar.close()
+        self._file.close()
 
     def write(self, data):
         """Write data."""
@@ -35,7 +60,3 @@ class SecureTarFile(object):
     def read(self, size=0):
         """Read data."""
         return self._aes.decrypt(self._file.read(size))
-
-    def close(self):
-        """Close file."""
-        self._file.close()
