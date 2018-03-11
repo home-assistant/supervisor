6d7312824c8bb5520c2ae9e5e2ae0111af6abe73
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-03-11 23:02:57 +0100

    Update aioHttp3

--- hassio/api/security.py
+++ hassio/api/security.py
@@ -6,6 +6,7 @@ from aiohttp.web import middleware
 from aiohttp.web_exceptions import HTTPUnauthorized
 
 from ..const import HEADER_TOKEN, REQUEST_FROM
+from ..coresys import CoreSysAttributes
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -16,35 +17,41 @@ NO_SECURITY_CHECK = set((
 ))
 
 
-@middleware
-async def security_layer(request, handler):
-    """Check security access of this layer."""
-    coresys = request.app['coresys']
-    hassio_token = request.headers.get(HEADER_TOKEN)
+class SecurityMiddleware(CoreSysAttributes):
+    """Security middleware functions."""
 
-    # Ignore security check
-    for rule in NO_SECURITY_CHECK:
-        if rule.match(request.path):
-            _LOGGER.debug("Passthrough %s", request.path)
+    def __init__(self, coresys):
+        """Initialize security middleware."""
+        self.coresys = coresys
+
+    @middleware
+    async def token_validation(self, request, handler):
+        """Check security access of this layer."""
+        hassio_token = request.headers.get(HEADER_TOKEN)
+
+        # Ignore security check
+        for rule in NO_SECURITY_CHECK:
+            if rule.match(request.path):
+                _LOGGER.debug("Passthrough %s", request.path)
+                return await handler(request)
+
+        # Need to be removed later
+        if not hassio_token:
+            _LOGGER.warning("Invalid token for access %s", request.path)
+            request[REQUEST_FROM] = 'UNKNOWN'
             return await handler(request)
 
-    # Need to be removed later
-    if not hassio_token:
-        _LOGGER.warning("Invalid token for access %s", request.path)
-        request[REQUEST_FROM] = 'UNKNOWN'
-        return await handler(request)
+        # Home-Assistant
+        if hassio_token == self._homeassistant.uuid:
+            _LOGGER.debug("%s access from Home-Assistant", request.path)
+            request[REQUEST_FROM] = 'homeassistant'
+            return await handler(request)
 
-    # Home-Assistant
-    if hassio_token == coresys.homeassistant.uuid:
-        _LOGGER.debug("%s access from Home-Assistant", request.path)
-        request[REQUEST_FROM] = 'homeassistant'
-        return await handler(request)
+        # Add-on
+        addon = self._addons.from_uuid(hassio_token)
+        if addon:
+            _LOGGER.info("%s access from %s", request.path, addon.slug)
+            request[REQUEST_FROM] = addon.slug
+            return await handler(request)
 
-    # Add-on
-    addon = coresys.addons.from_uuid(hassio_token)
-    if addon:
-        _LOGGER.info("%s access from %s", request.path, addon.slug)
-        request[REQUEST_FROM] = addon.slug
-        return await handler(request)
-
-    raise HTTPUnauthorized()
+        raise HTTPUnauthorized()
