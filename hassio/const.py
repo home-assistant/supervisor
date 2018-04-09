1374f904334bd61789e3e75667593d6a02314afa
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-04-09 22:52:16 +0200

    cleanup version

--- hassio/const.py
+++ hassio/const.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 from ipaddress import ip_network
 
-HASSIO_VERSION = '1.00'
+HASSIO_VERSION = '1.0'
 
 URL_HASSIO_VERSION = ('https://raw.githubusercontent.com/home-assistant/'
                       'hassio/{}/version.json')
