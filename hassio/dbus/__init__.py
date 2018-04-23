a4ba4c80e860684e1815aabbd1a120b58024274a
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-04-23 08:57:39 +0200

    Update __init__.py

--- hassio/dbus/__init__.py
+++ hassio/dbus/__init__.py
@@ -1,1 +1,20 @@
 """DBus interface objects."""
+
+from .systemd import Systemd
+
+
+class DBusManager(CoreSysAttributes):
+    """DBus Interface handler."""
+
+    def __init__(self):
+        """Initialize DBus  Interface."""
+        self._systemd = Systemd()
+
+    @property
+    def systemd(self):
+        """Return Systemd Interface."""
+        return self._systemd
+
+    async def load(self):
+        """Connect interfaces to dbus."""
+        await self.systemd.connect()
