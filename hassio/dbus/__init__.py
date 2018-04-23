6f770b78afa9876eb683d3eeb0bfd1efb53c8eb0
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-04-23 23:30:21 +0200

    add interface dbus class

--- hassio/dbus/__init__.py
+++ hassio/dbus/__init__.py
@@ -1,6 +1,7 @@
 """DBus interface objects."""
 
 from .systemd import Systemd
+from .hostname import Hostname
 from ..coresys import CoreSysAttributes
 
 
@@ -11,12 +12,19 @@ class DBusManager(CoreSysAttributes):
         """Initialize DBus  Interface."""
         self.coresys = coresys
         self._systemd = Systemd()
+        self._hostname = Hostname()
 
     @property
     def systemd(self):
         """Return Systemd Interface."""
         return self._systemd
 
+    @property
+    def hostname(self):
+        """Return hostname Interface."""
+        return self._hostname
+
     async def load(self):
         """Connect interfaces to dbus."""
         await self.systemd.connect()
+        await self.hostname.connect()
--- hassio/dbus/hostname.py
+++ hassio/dbus/hostname.py
@@ -1,1 +1,23 @@
 """DBus interface for hostname."""
+import logging
+
+from .interface import DBusInterface
+from .utils import dbus_connected
+from ..exceptions import DBusError
+from ..utils.gdbus import DBus
+
+_LOGGER = logging.getLogger(__name__)
+
+DBUS_NAME = 'org.freedesktop.hostname1'
+DBUS_OBJECT = '/org/freedesktop/hostname1'
+
+
+class Hostname(DBusInterface):
+    """Handle DBus interface for hostname/system."""
+
+    async def connect(self):
+        """Connect do bus."""
+        try:
+            self.dbus = await DBus.connect(DBUS_NAME, DBUS_OBJECT)
+        except DBusError:
+            _LOGGER.warning("Can't connect to hostname")
