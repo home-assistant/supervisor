82f68b4a7be2b082d1501a2023c6b487f2649a27
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-04-25 22:27:57 +0200

    fix dbus

--- hassio/dbus/utils.py
+++ hassio/dbus/utils.py
@@ -5,10 +5,10 @@ from ..exceptions import DBusNotConnectedError
 
 def dbus_connected(method):
     """Wrapper for check if dbus is connected."""
-    def wrap_dbus(self, *args, **kwargs):
+    def wrap_dbus(api, *args, **kwargs):
         """Check if dbus is connected before call a method."""
-        if self.dbus is None:
-            raise DBusNotConnectedError(f"{self!s} not connected to dbus!")
-        return self.method(*args, **kwargs)
+        if api.dbus is None:
+            raise DBusNotConnectedError(f"{api!s} not connected to dbus!")
+        return method(api, *args, **kwargs)
 
     return wrap_dbus
