20afa1544bbb9b5f2e04b1f84127c3cb1f0ee794
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-05-29 00:22:12 +0200

    Bump version to 105

--- hassio/const.py
+++ hassio/const.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 from ipaddress import ip_network
 
-HASSIO_VERSION = '104'
+HASSIO_VERSION = '105'
 
 URL_HASSIO_VERSION = ('https://raw.githubusercontent.com/home-assistant/'
                       'hassio/{}/version.json')
