0c855dc841b5b76702a7abd64c9ccf975aaf8517
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-06-15 16:38:48 +0200

    Update alsa.py

--- hassio/host/alsa.py
+++ hassio/host/alsa.py
@@ -81,7 +81,7 @@ class AlsaAudio(CoreSysAttributes):
     @staticmethod
     def _audio_database():
         """Read local json audio data into dict."""
-        json_file = Path(__file__).parent.joinpath('data/audiodb.json')
+        json_file = Path(__file__).parent.joinpath("data/audiodb.json")
 
         try:
             # pylint: disable=no-member
@@ -121,7 +121,7 @@ class AlsaAudio(CoreSysAttributes):
         alsa_output = alsa_output or self.default.output
 
         # Read Template
-        asound_file = Path(__file__).parent.joinpath('data/asound.tmpl')
+        asound_file = Path(__file__).parent.joinpath("data/asound.tmpl")
         try:
             # pylint: disable=no-member
             with asound_file.open('r') as asound:
