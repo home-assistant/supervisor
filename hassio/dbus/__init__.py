5fe9d63c79b22b95d7a54415abb363ed4f5c2a30
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-06-30 01:48:58 +0200

    Add HassOS OTA support on Hass.io (#536)
    * Add HassOS OTA support on Hass.io
    
    * Update dt.py
    
    * Update updater.py
    
    * add rauc dbus / initial dbus signal handling
    
    * Update gdbus.py
    
    * Update hassos.py
    
    * Update const.py
    
    * Update hassos.py
    
    * Update exceptions.py
    
    * Update hassos.py
    
    * Update rauc.py
    
    * Update rauc.py
    
    * Update rauc.py
    
    * Update hassos.py
    
    * Update hassos.py
    
    * Update hassos.py
    
    * Update hassos.py
    
    * Update hassos.py
    
    * Update hassos.py
    
    * Update hassos.py
    
    * Update __init__.py
    
    * Update hassos.py
    
    * Update hassos.py
    
    * Update updater.py
    
    * Update updater.py
    
    * Update exceptions.py
    
    * Update exceptions.py
    
    * Update hassos.py
    
    * Update dt.py
    
    * fix lint
    
    * Fix update
    
    * fix property
    
    * tmp disabled
    
    * fix path
    
    * fix rauc
    
    * info
    
    * More details
    
    * cleanup signal hadnling
    
    * fix
    
    * Fix lint

--- hassio/dbus/__init__.py
+++ hassio/dbus/__init__.py
@@ -2,6 +2,7 @@
 
 from .systemd import Systemd
 from .hostname import Hostname
+from .rauc import Rauc
 from ..coresys import CoreSysAttributes
 
 
@@ -11,8 +12,10 @@ class DBusManager(CoreSysAttributes):
     def __init__(self, coresys):
         """Initialize DBus  Interface."""
         self.coresys = coresys
+
         self._systemd = Systemd()
         self._hostname = Hostname()
+        self._rauc = Rauc()
 
     @property
     def systemd(self):
@@ -24,7 +27,13 @@ class DBusManager(CoreSysAttributes):
         """Return hostname Interface."""
         return self._hostname
 
+    @property
+    def rauc(self):
+        """Return rauc Interface."""
+        return self._rauc
+
     async def load(self):
         """Connect interfaces to dbus."""
         await self.systemd.connect()
         await self.hostname.connect()
+        await self.rauc.connect()
