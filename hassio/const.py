60ba2db561152bb44c00d575cdf8d6a29a350b7f
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-07-03 20:44:06 +0200

    Bump version to 112

--- hassio/const.py
+++ hassio/const.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 from ipaddress import ip_network
 
-HASSIO_VERSION = '111'
+HASSIO_VERSION = '112'
 
 URL_HASSIO_ADDONS = "https://github.com/home-assistant/hassio-addons"
 URL_HASSIO_VERSION = \
