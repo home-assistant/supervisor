241f4e412b9c58cf0c144e3a130cf9c244547cc7
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-07-06 00:19:49 +0200

    Fix dbus call

--- hassio/dbus/hostname.py
+++ hassio/dbus/hostname.py
@@ -28,7 +28,7 @@ class Hostname(DBusInterface):
 
         Return a coroutine.
         """
-        return self.dbus.SetStaticHostname(hostname)
+        return self.dbus.SetStaticHostname(hostname, 0)
 
     @dbus_connected
     def get_properties(self):
