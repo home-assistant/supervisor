b4adcb3ba5710b46f71193ac0ff3b885540506e0
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-09-06 16:16:18 +0200

    Update security.py

--- hassio/api/security.py
+++ hassio/api/security.py
@@ -10,11 +10,21 @@ from ..coresys import CoreSysAttributes
 
 _LOGGER = logging.getLogger(__name__)
 
-NO_SECURITY_CHECK = set((
-    re.compile(r"^/homeassistant/api/.*$"),
-    re.compile(r"^/homeassistant/websocket$"),
-    re.compile(r"^/supervisor/ping$"),
-))
+NO_SECURITY_CHECK = re.compile(
+    r"^(?:",
+    r"|/homeassistant/api/.*$",
+    r"|/homeassistant/websocket$",
+    r"|/supervisor/ping$",
+    r")$"
+)
+
+ADDONS_API_BYPASS = re.compile(
+    r"^(?:",
+    r"|/homeassistant/info$",
+    r"|/supervisor/info$",
+    r"|"^/addons(?:/self/[^/]+)?$",
+    r")$"
+)
 
 
 class SecurityMiddleware(CoreSysAttributes):
@@ -27,32 +37,37 @@ class SecurityMiddleware(CoreSysAttributes):
     @middleware
     async def token_validation(self, request, handler):
         """Check security access of this layer."""
+        request_from = None
         hassio_token = request.headers.get(HEADER_TOKEN)
 
         # Ignore security check
-        for rule in NO_SECURITY_CHECK:
-            if rule.match(request.path):
-                _LOGGER.debug("Passthrough %s", request.path)
-                return await handler(request)
+        if NO_SECURITY_CHECK.match(request.path):
+            _LOGGER.debug("Passthrough %s", request.path)
+            return await handler(request)
 
         # Home-Assistant
         if hassio_token == self.sys_homeassistant.uuid:
             _LOGGER.debug("%s access from Home-Assistant", request.path)
-            request[REQUEST_FROM] = 'homeassistant'
+            request_from = 'homeassistant'
 
         # Host
         if hassio_token == self.sys_machine_id:
             _LOGGER.debug("%s access from Host", request.path)
-            request[REQUEST_FROM] = 'host'
+            request_from = 'host'
 
         # Add-on
-        addon = self.sys_addons.from_uuid(hassio_token) \
-            if hassio_token else None
-        if addon:
+        if hassio_token and not request_from:
+            addon = self.sys_addons.from_uuid(hassio_token)
+
+        if addon and addon.with_hassio_api:
             _LOGGER.info("%s access from %s", request.path, addon.slug)
-            request[REQUEST_FROM] = addon.slug
+            request_from = addon.slug
+        elif addon and ADDONS_API_BYPASS.match(request.path):
+            _LOGGER.debug("Passthrough %s from %s", request.path, addon.slug)
+            request_from = addon.slug
 
         if request.get(REQUEST_FROM):
+            request[REQUEST_FROM] = request_from
             return await handler(request)
 
         _LOGGER.warning("Invalid token for access %s", request.path)
