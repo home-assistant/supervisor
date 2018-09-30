6b4b6d6cc46416454e6ac90b9c53024ee00297e1
Pascal Vizeli <pvizeli@syshack.ch>
2018-09-30 15:21:51 +0200

    fix validate

--- hassio/validate.py
+++ hassio/validate.py
@@ -111,7 +111,7 @@ SCHEMA_HASSIO_CONFIG = vol.Schema({
 
 SCHEMA_DISCOVERY = vol.Schema([
     vol.Schema({
-        vol.Required(ATTR_UUID): vol.Match(UUID_MATCH),
+        vol.Required(ATTR_UUID): UUID_MATCH,
         vol.Required(ATTR_ADDON): vol.Coerce(str),
         vol.Required(ATTR_SERVICE): SERVICE_ALL,
         vol.Required(ATTR_COMPONENT): vol.Coerce(str),
