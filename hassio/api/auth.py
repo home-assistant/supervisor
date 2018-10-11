249d752ed3f36a8534d919a9d91b608c9b530396
Pascal Vizeli <pvizeli@syshack.ch>
2018-10-11 16:32:16 +0200

    Update auth.py

--- hassio/api/auth.py
+++ hassio/api/auth.py
@@ -6,7 +6,7 @@ from aiohttp import BasicAuth
 from aiohttp.hdrs import CONTENT_TYPE, AUTHORIZATION
 
 from .utils import api_process
-from ..const import REQUEST_FROM, CONTENT_TYPE_JSON
+from ..const import REQUEST_FROM, CONTENT_TYPE_JSON, CONTENT_TYPE_URL
 from ..coresys import CoreSysAttributes
 from ..exceptions import APIError, APIForbidden
 
@@ -24,20 +24,15 @@ class APIAuth(CoreSysAttributes):
         auth = BasicAuth.decode(request.headers[AUTHORIZATION])
         return self.sys_auth.check_login(addon, auth.login, auth.password)
 
-    def async _process_json_auth(self, request, addon):
-        """Process login with json reuqest."""
-        data = await request.json()
-
-        username = data.get('username') or data.get('user')
-        password = data.get('password')
-
-        return await self.sys_auth.check_login(addon, username, password)
-
-    def _process_post_auth(self, request, addon):
-        """Process login with post variable reuqest.
+    def _process_dict_auth(self, request, addon, data):
+        """Process login with dict data.
         
         Return a coroutine.
         """
+        username = data.get('username') or data.get('user')
+        password = data.get('password')
+
+        return self.sys_auth.check_login(addon, username, password)
 
     @api_process
     async def auth(self, request):
@@ -49,9 +44,16 @@ class APIAuth(CoreSysAttributes):
 
         # BasicAuth
         if AUTHORIZATION in request.headers:
-            return await _process_basic_auth(self, request)
+            return await _process_basic_auth(self, request, addon)
 
         # Json
         if request.headers[CONTENT_TYPE] == CONTENT_TYPE_JSON:
-            return await _process_json_auth(self, request)
+            data = await request.json()
+            return await _process_dict_auth(self, request, addon, data)
  
+        # URL encoded
+        if request.headers[CONTENT_TYPE] == CONTENT_TYPE_URL:
+            data = await request.post()
+            return await _process_dict_auth(self, request, addon, data)
+
+        raise APIError("Auth method not detected!")
