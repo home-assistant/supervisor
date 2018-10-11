dfe9afe28c5f3058f3b49553e97f5f1d2737be20
Pascal Vizeli <pvizeli@syshack.ch>
2018-10-11 16:22:34 +0200

    Update auth.py

--- hassio/api/auth.py
+++ hassio/api/auth.py
@@ -1,8 +1,12 @@
 """Init file for Hass.io auth/SSO RESTful API."""
 import logging
+import json
+
+from aiohttp import BasicAuth
+from aiohttp.hdrs import CONTENT_TYPE, AUTHORIZATION
 
 from .utils import api_process
-from ..const import REQUEST_FROM
+from ..const import REQUEST_FROM, CONTENT_TYPE_JSON
 from ..coresys import CoreSysAttributes
 from ..exceptions import APIError, APIForbidden
 
@@ -12,14 +16,28 @@ _LOGGER = logging.getLogger(__name__)
 class APIAuth(CoreSysAttributes):
     """Handle RESTful API for auth functions."""
 
-    async def _process_basic_auth(self, request):
-        """Process login request with basic auth."""
+    def _process_basic_auth(self, request, addon):
+        """Process login request with basic auth.
+        
+        Return a coroutine.
+        """
+        auth = BasicAuth.decode(request.headers[AUTHORIZATION])
+        return self.sys_auth.check_login(addon, auth.login, auth.password)
 
-    async def _process_json_auth(self, request):
+    def async _process_json_auth(self, request, addon):
         """Process login with json reuqest."""
+        data = await request.json()
 
-    async def _process_post_auth(self, request):
-        """Process login with post variable reuqest."""
+        username = data.get('username') or data.get('user')
+        password = data.get('password')
+
+        return await self.sys_auth.check_login(addon, username, password)
+
+    def _process_post_auth(self, request, addon):
+        """Process login with post variable reuqest.
+        
+        Return a coroutine.
+        """
 
     @api_process
     async def auth(self, request):
@@ -29,4 +47,11 @@ class APIAuth(CoreSysAttributes):
         if not addon.with_login_backend:
             raise APIForbidden("Can't use Home Assistant auth!")
 
-        if Authorization
+        # BasicAuth
+        if AUTHORIZATION in request.headers:
+            return await _process_basic_auth(self, request)
+
+        # Json
+        if request.headers[CONTENT_TYPE] == CONTENT_TYPE_JSON:
+            return await _process_json_auth(self, request)
+ 
