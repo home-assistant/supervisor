299cb907cd7a46db2e70a870c3a6269a7c6994f2
Pascal Vizeli <pvizeli@syshack.ch>
2018-10-11 14:33:49 +0200

    Update auth.py

--- hassio/api/auth.py
+++ hassio/api/auth.py
@@ -11,9 +11,15 @@ _LOGGER = logging.getLogger(__name__)
 
 class APIAuth(CoreSysAttributes):
     """Handle RESTful API for auth functions."""
-    
-    async _process_basic_auth(self):
-        """Process login request w
+
+    async def _process_basic_auth(self, request):
+        """Process login request with basic auth."""
+
+    async def _process_json_auth(self, request):
+        """Process login with json reuqest."""
+
+    async def _process_post_auth(self, request):
+        """Process login with post variable reuqest."""
 
     @api_process
     async def auth(self, request):
@@ -22,3 +28,5 @@ class APIAuth(CoreSysAttributes):
 
         if not addon.with_login_backend:
             raise APIForbidden("Can't use Home Assistant auth!")
+
+        if Authorization
