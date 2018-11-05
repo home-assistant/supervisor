06c26dfaea3e4554c9dd3af58bfc42b5c6285aee
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-11-05 14:31:53 +0100

    Update discovery.py

--- hassio/discovery.py
+++ hassio/discovery.py
@@ -115,7 +115,7 @@ class Discovery(CoreSysAttributes, JsonConfig):
 @attr.s
 class Message:
     """Represent a single Discovery message."""
-    uuid = attr.ib(factory=lambda: uuid4().hex, kw_only=True, cmp=False)
     addon = attr.ib()
     service = attr.ib()
     config = attr.ib(cmp=False)
+    uuid = attr.ib(factory=lambda: uuid4().hex, kw_only=True, cmp=False)
