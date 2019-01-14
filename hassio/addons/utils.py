aa1c765c4be0428ef79b7b755c70b086b3b567f9
Pascal Vizeli <pascal.vizeli@syshack.ch>
2019-01-15 00:56:07 +0100

    Add support for SYS_MODULE (#889)
    * Add support for SYS_MODULE
    
    * Update flake stuff
    
    * Fix lint
    
    * Fix lint
    
    * Fix lint
    
    * Fix lint

--- hassio/addons/utils.py
+++ hassio/addons/utils.py
@@ -7,7 +7,8 @@ import re
 from ..const import (
     SECURITY_DISABLE, SECURITY_PROFILE, PRIVILEGED_NET_ADMIN,
     PRIVILEGED_SYS_ADMIN, PRIVILEGED_SYS_RAWIO, PRIVILEGED_SYS_PTRACE,
-    PRIVILEGED_DAC_READ_SEARCH, ROLE_ADMIN, ROLE_MANAGER)
+    PRIVILEGED_DAC_READ_SEARCH, PRIVILEGED_SYS_MODULE, ROLE_ADMIN,
+    ROLE_MANAGER)
 
 RE_SHA1 = re.compile(r"[a-f0-9]{8}")
 
@@ -33,10 +34,17 @@ def rating_security(addon):
         rating += 1
 
     # Privileged options
-    if any(privilege in addon.privileged
-           for privilege in (PRIVILEGED_NET_ADMIN, PRIVILEGED_SYS_ADMIN,
-                             PRIVILEGED_SYS_RAWIO, PRIVILEGED_SYS_PTRACE,
-                             PRIVILEGED_DAC_READ_SEARCH)):
+    if any(
+            privilege in addon.privileged
+            for privilege in (
+                    PRIVILEGED_NET_ADMIN,
+                    PRIVILEGED_SYS_ADMIN,
+                    PRIVILEGED_SYS_RAWIO,
+                    PRIVILEGED_SYS_PTRACE,
+                    PRIVILEGED_SYS_MODULE,
+                    PRIVILEGED_DAC_READ_SEARCH,
+            )
+    ):
         rating += -1
 
     # API Hass.io role
@@ -81,6 +89,7 @@ def extract_hash_from_path(path):
 
 def check_installed(method):
     """Wrap function with check if add-on is installed."""
+
     async def wrap_check(addon, *args, **kwargs):
         """Return False if not installed or the function."""
         if not addon.is_installed:
@@ -95,8 +104,7 @@ async def remove_data(folder):
     """Remove folder and reset privileged."""
     try:
         proc = await asyncio.create_subprocess_exec(
-            "rm", "-rf", str(folder),
-            stdout=asyncio.subprocess.DEVNULL
+            "rm", "-rf", str(folder), stdout=asyncio.subprocess.DEVNULL
         )
 
         _, error_msg = await proc.communicate()
