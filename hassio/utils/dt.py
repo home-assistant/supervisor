acddfe3a4281886cd23e3eec045347a40a34dfc6
Pascal Vizeli <pvizeli@syshack.ch>
2019-02-19 09:53:30 +0100

    Fix lint

--- hassio/utils/dt.py
+++ hassio/utils/dt.py
@@ -2,6 +2,7 @@
 from datetime import datetime, timedelta, timezone
 import logging
 import re
+from typing import Any, Dict, Optional
 
 import pytz
 
@@ -14,10 +15,10 @@ _LOGGER = logging.getLogger(__name__)
 # All rights reserved.
 # https://github.com/django/django/blob/master/LICENSE
 DATETIME_RE = re.compile(
-    r'(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})'
-    r'[T ](?P<hour>\d{1,2}):(?P<minute>\d{1,2})'
-    r'(?::(?P<second>\d{1,2})(?:\.(?P<microsecond>\d{1,6})\d{0,6})?)?'
-    r'(?P<tzinfo>Z|[+-]\d{2}(?::?\d{2})?)?$'
+    r"(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})"
+    r"[T ](?P<hour>\d{1,2}):(?P<minute>\d{1,2})"
+    r"(?::(?P<second>\d{1,2})(?:\.(?P<microsecond>\d{1,6})\d{0,6})?)?"
+    r"(?P<tzinfo>Z|[+-]\d{2}(?::?\d{2})?)?$"
 )
 
 
@@ -35,25 +36,25 @@ def parse_datetime(dt_str):
     match = DATETIME_RE.match(dt_str)
     if not match:
         return None
-    kws = match.groupdict()  # type: Dict[str, Any]
-    if kws['microsecond']:
-        kws['microsecond'] = kws['microsecond'].ljust(6, '0')
-    tzinfo_str = kws.pop('tzinfo')
+    kws: Dict[str, Any] = match.groupdict()
+    if kws["microsecond"]:
+        kws["microsecond"] = kws["microsecond"].ljust(6, "0")
+    tzinfo_str = kws.pop("tzinfo")
 
-    tzinfo = None  # type: Optional[dt.tzinfo]
-    if tzinfo_str == 'Z':
+    tzinfo: Optional[dt.tzinfo] = None
+    if tzinfo_str == "Z":
         tzinfo = UTC
     elif tzinfo_str is not None:
         offset_mins = int(tzinfo_str[-2:]) if len(tzinfo_str) > 3 else 0
         offset_hours = int(tzinfo_str[1:3])
         offset = timedelta(hours=offset_hours, minutes=offset_mins)
-        if tzinfo_str[0] == '-':
+        if tzinfo_str[0] == "-":
             offset = -offset
         tzinfo = timezone(offset)
     else:
         tzinfo = None
     kws = {k: int(v) for k, v in kws.items() if v is not None}
-    kws['tzinfo'] = tzinfo
+    kws["tzinfo"] = tzinfo
     return datetime(**kws)
 
 
