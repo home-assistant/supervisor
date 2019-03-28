b57c9215c1a047896e48ed1a308dea7da1381961
Pascal Vizeli <pascal.vizeli@syshack.ch>
2019-03-28 14:13:36 +0100

    Remove unused pylint

--- hassio/discovery/services/deconz.py
+++ hassio/discovery/services/deconz.py
@@ -5,7 +5,7 @@ from hassio.validate import NETWORK_PORT
 
 from ..const import ATTR_HOST, ATTR_PORT
 
-# pylint: disable=no-value-for-parameter
+
 SCHEMA = vol.Schema(
     {vol.Required(ATTR_HOST): vol.Coerce(str), vol.Required(ATTR_PORT): NETWORK_PORT}
 )
