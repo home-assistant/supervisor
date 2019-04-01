aa151df18153862cf4d947b7043df5ad4f05533b
Pascal Vizeli <pascal.vizeli@syshack.ch>
2019-04-01 16:58:58 +0200

    Update API data for deconz (#989)
    * Update API data for deconz
    
    * Fix tests

--- hassio/discovery/services/deconz.py
+++ hassio/discovery/services/deconz.py
@@ -3,9 +3,14 @@ import voluptuous as vol
 
 from hassio.validate import NETWORK_PORT
 
-from ..const import ATTR_HOST, ATTR_PORT
+from ..const import ATTR_HOST, ATTR_PORT, ATTR_API_KEY, ATTR_SERIAL
 
 
 SCHEMA = vol.Schema(
-    {vol.Required(ATTR_HOST): vol.Coerce(str), vol.Required(ATTR_PORT): NETWORK_PORT}
+    {
+        vol.Required(ATTR_HOST): vol.Coerce(str),
+        vol.Required(ATTR_PORT): NETWORK_PORT,
+        vol.Required(ATTR_SERIAL): vol.Coerce(str),
+        vol.Required(ATTR_API_KEY): vol.Coerce(str),
+    }
 )
