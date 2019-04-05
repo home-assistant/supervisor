f10b433e1f0aab92966e6ac449541fcb3ef11383
Pascal Vizeli <pascal.vizeli@syshack.ch>
2019-04-05 17:49:43 +0200

    Fix token handling with new secrets (#996)
    * Fix token handling with new secrets
    
    * add schema also to ingress

--- hassio/validate.py
+++ hassio/validate.py
@@ -40,6 +40,7 @@ ALSA_DEVICE = vol.Maybe(vol.Match(r"\d+,\d+"))
 CHANNELS = vol.In([CHANNEL_STABLE, CHANNEL_BETA, CHANNEL_DEV])
 UUID_MATCH = vol.Match(r"^[0-9a-f]{32}$")
 SHA256 = vol.Match(r"^[0-9a-f]{64}$")
+TOKEN = vol.Match(r"^[0-9a-f]{32,256}$")
 
 
 def validate_repository(repository):
@@ -94,7 +95,7 @@ DOCKER_PORTS = vol.Schema(
 SCHEMA_HASS_CONFIG = vol.Schema(
     {
         vol.Optional(ATTR_UUID, default=lambda: uuid.uuid4().hex): UUID_MATCH,
-        vol.Optional(ATTR_ACCESS_TOKEN): SHA256,
+        vol.Optional(ATTR_ACCESS_TOKEN): TOKEN,
         vol.Optional(ATTR_BOOT, default=True): vol.Boolean(),
         vol.Inclusive(ATTR_IMAGE, "custom_hass"): DOCKER_IMAGE,
         vol.Inclusive(ATTR_LAST_VERSION, "custom_hass"): vol.Coerce(str),
@@ -142,9 +143,6 @@ SCHEMA_AUTH_CONFIG = vol.Schema({SHA256: SHA256})
 
 
 SCHEMA_INGRESS_CONFIG = vol.Schema(
-    {
-        vol.Required(ATTR_SESSION, default=dict): vol.Schema(
-            {vol.Coerce(str): vol.Coerce(float)}
-        )
-    }
+    {vol.Required(ATTR_SESSION, default=dict): vol.Schema({TOKEN: vol.Coerce(float)})},
+    extra=vol.REMOVE_EXTRA,
 )
