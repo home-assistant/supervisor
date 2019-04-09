b77146a4e0052caaf24bc9c71831e5db3189c1d6
Pascal Vizeli <pascal.vizeli@syshack.ch>
2019-04-09 22:15:23 +0200

    Allow to add a description for a port (#1023)

--- hassio/validate.py
+++ hassio/validate.py
@@ -61,34 +61,16 @@ def validate_repository(repository):
 REPOSITORIES = vol.All([validate_repository], vol.Unique())
 
 
-# pylint: disable=inconsistent-return-statements
-def convert_to_docker_ports(data):
-    """Convert data into Docker port list."""
-    # dynamic ports
-    if data is None:
-        return None
-
-    # single port
-    if isinstance(data, int):
-        return NETWORK_PORT(data)
-
-    # port list
-    if isinstance(data, list) and len(data) > 2:
-        return vol.Schema([NETWORK_PORT])(data)
-
-    # ip port mapping
-    if isinstance(data, list) and len(data) == 2:
-        return (vol.Coerce(str)(data[0]), NETWORK_PORT(data[1]))
-
-    raise vol.Invalid("Can't validate Docker host settings")
-
-
 DOCKER_PORTS = vol.Schema(
     {
-        vol.All(
-            vol.Coerce(str), vol.Match(r"^\d+(?:/tcp|/udp)?$")
-        ): convert_to_docker_ports
+        vol.All(vol.Coerce(str), vol.Match(r"^\d+(?:/tcp|/udp)?$")): vol.Maybe(
+            NETWORK_PORT
+        )
     }
+)
+
+DOCKER_PORTS_DESCRIPTION = vol.Schema(
+    {vol.All(vol.Coerce(str), vol.Match(r"^\d+(?:/tcp|/udp)?$")): vol.Coerce(str)}
 )
 
 
