e1ea2a56284ab866fb95f67309ede9be4e3f625d
Pascal Vizeli <pvizeli@syshack.ch>
2019-05-18 22:39:43 +0200

    Bump version 165

--- hassio/const.py
+++ hassio/const.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from ipaddress import ip_network
 
 
-HASSIO_VERSION = "164"
+HASSIO_VERSION = "165"
 
 URL_HASSIO_ADDONS = "https://github.com/home-assistant/hassio-addons"
 URL_HASSIO_VERSION = "https://s3.amazonaws.com/hassio-version/{channel}.json"
