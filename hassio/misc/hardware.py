c0342323836dcb2101cefa7510ae90c71f74f4e8
Pascal Vizeli <pvizeli@syshack.ch>
2019-08-14 10:18:59 +0000

    upgrade logger

--- hassio/misc/hardware.py
+++ hassio/misc/hardware.py
@@ -85,7 +85,7 @@ class Hardware:
     def audio_devices(self) -> Dict[str, Any]:
         """Return all available audio interfaces."""
         if not ASOUND_CARDS.exists():
-            _LOGGER.debug("No audio devices found")
+            _LOGGER.info("No audio devices found")
             return {}
 
         try:
