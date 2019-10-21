e701e9a24fa4818899da8b3204dcc71adfc94744
Pascal Vizeli <pvizeli@syshack.ch>
2019-10-21 09:44:24 +0000

    Add tests

---
+++ tests/utils/test_tarfile.py
@@ -0,0 +1,33 @@
+"""Test Tarfile functions."""
+
+import attr
+
+from hassio.utils.tar import secure_path
+
+
+@attr.s
+class TarInfo:
+    """Fake TarInfo"""
+
+    name: str = attr.ib()
+
+
+def test_secure_path():
+    """Test Secure Path."""
+    test_list = [
+        TarInfo("test.txt"),
+        TarInfo("data/xy.blob"),
+        TarInfo("bla/blu/ble"),
+        TarInfo("data/../xy.blob"),
+    ]
+    assert test_list == list(secure_path(test_list))
+
+
+def test_not_secure_path():
+    """Test Not secure path."""
+    test_list = [
+        TarInfo("/test.txt"),
+        TarInfo("data/../../xy.blob"),
+        TarInfo("/bla/blu/ble"),
+    ]
+    assert [] == list(secure_path(test_list))
