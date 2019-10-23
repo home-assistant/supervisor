df3e9e3a5e34beb4ef265b9652378f19cd9a12a5
Pascal Vizeli <pvizeli@syshack.ch>
2019-10-23 16:02:42 +0200

    Bump version 192

--- hassio/const.py
+++ hassio/const.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 from ipaddress import ip_network
 
-HASSIO_VERSION = "191"
+HASSIO_VERSION = "192"
 
 
 URL_HASSIO_ADDONS = "https://github.com/home-assistant/hassio-addons"
