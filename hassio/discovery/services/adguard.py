fc5d97562f1abd028295542e602f04f9ce055dce
Pascal Vizeli <pvizeli@syshack.ch>
2019-12-05 21:52:55 +0100

    Dns update (#1393)
    * Improvements to DNS validator to include IPv6 (#1312)
    
    * improvements to DNS validator to include IPv6
    
    * fixed the DNS validators
    
    * updated per suggestions
    
    * Update const.py
    
    * Update dns.py
    
    * Update validate.py
    
    * Update validate.py
    
    * Update dns.py
    
    * Update test_validate.py
    
    * Update validate.py
    
    * Cleanup
    
    * Don't set default DNS server as default
    
    * Remove update local resolver
    
    * Fix lint

--- hassio/discovery/services/adguard.py
+++ hassio/discovery/services/adguard.py
@@ -1,11 +1,11 @@
 """Discovery service for AdGuard."""
 import voluptuous as vol
 
-from hassio.validate import NETWORK_PORT
+from hassio.validate import network_port
 
 from ..const import ATTR_HOST, ATTR_PORT
 
 
 SCHEMA = vol.Schema(
-    {vol.Required(ATTR_HOST): vol.Coerce(str), vol.Required(ATTR_PORT): NETWORK_PORT}
+    {vol.Required(ATTR_HOST): vol.Coerce(str), vol.Required(ATTR_PORT): network_port}
 )
