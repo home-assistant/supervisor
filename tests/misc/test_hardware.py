8dbfea75b1d80f50a72c1eb8c487457e3ed567d2
Pascal Vizeli <pvizeli@syshack.ch>
2020-02-20 00:29:48 +0100

    Extend video & add tests (#1518)

--- tests/misc/test_hardware.py
+++ tests/misc/test_hardware.py
@@ -1,6 +1,8 @@
 """Test hardware utils."""
+from unittest.mock import patch, PropertyMock
+from pathlib import Path
 
-from hassio.misc.hardware import Hardware
+from hassio.misc.hardware import Hardware, Device
 
 
 def test_read_all_devices():
@@ -8,3 +10,25 @@ def test_read_all_devices():
     system = Hardware()
 
     assert system.devices
+
+
+def test_video_devices():
+    """Test video device filter."""
+    system = Hardware()
+    device_list = [
+        Device("test-dev", Path("/dev/test-dev"), []),
+        Device("vchiq", Path("/dev/vchiq"), []),
+        Device("cec0", Path("/dev/cec0"), []),
+        Device("video1", Path("/dev/video1"), []),
+    ]
+
+    with patch(
+        "hassio.misc.hardware.Hardware.devices", new_callable=PropertyMock
+    ) as mock_device:
+        mock_device.return_value = device_list
+
+        assert system.video_devices == [
+            Device("vchiq", Path("/dev/vchiq"), []),
+            Device("cec0", Path("/dev/cec0"), []),
+            Device("video1", Path("/dev/video1"), []),
+        ]
