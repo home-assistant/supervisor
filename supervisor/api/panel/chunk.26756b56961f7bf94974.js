2be9996852770931146a17a3e2dae868342c886e
Pascal Vizeli <pvizeli@syshack.ch>
2020-02-27 12:43:03 +0000

    Update Panel for audio

---
+++ supervisor/api/panel/chunk.26756b56961f7bf94974.js
@@ -0,0 +1,2 @@
+(self.webpackJsonp=self.webpackJsonp||[]).push([[2],{177:function(e,r,n){"use strict";n.r(r),n.d(r,"codeMirror",function(){return c}),n.d(r,"codeMirrorCss",function(){return i});var a=n(54),o=n.n(a),s=n(170),t=(n(171),n(172),n(11));o.a.commands.save=function(e){Object(t.a)(e.getWrapperElement(),"editor-save")};var c=o.a,i=s.a}}]);
+//# sourceMappingURL=chunk.26756b56961f7bf94974.js.map
