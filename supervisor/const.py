4fe5d09f01ba9978148501e61ac263fe47dd7168
Pascal Vizeli <pvizeli@syshack.ch>
2020-02-29 12:19:42 +0100

    Bump version to 206

--- supervisor/const.py
+++ supervisor/const.py
@@ -3,7 +3,7 @@ from enum import Enum
 from ipaddress import ip_network
 from pathlib import Path
 
-SUPERVISOR_VERSION = "205"
+SUPERVISOR_VERSION = "206"
 
 
 URL_HASSIO_ADDONS = "https://github.com/home-assistant/hassio-addons"
