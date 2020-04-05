83d398b537a52d134a62b8f1f9ada414e47b1284
Pascal Vizeli <pvizeli@syshack.ch>
2020-04-05 10:38:16 +0000

    support network forward

--- supervisor/docker/multicast.py
+++ supervisor/docker/multicast.py
@@ -2,10 +2,10 @@
 from contextlib import suppress
 import logging
 
+from ..const import DOCKER_NETWORK_MASK, ENV_HASSIO_NETWORK, ENV_TIME
 from ..coresys import CoreSysAttributes
 from ..exceptions import DockerAPIError
 from .interface import DockerInterface
-from ..const import ENV_TIME
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
 
@@ -47,7 +47,10 @@ class DockerMulticast(DockerInterface, CoreSysAttributes):
             network_mode="host",
             detach=True,
             extra_hosts={"supervisor": self.sys_docker.network.supervisor},
-            environment={ENV_TIME: self.sys_timezone},
+            environment={
+                ENV_TIME: self.sys_timezone,
+                ENV_HASSIO_NETWORK: str(DOCKER_NETWORK_MASK),
+            },
         )
 
         self._meta = docker_container.attrs
