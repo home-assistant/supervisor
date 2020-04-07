cb9f54918b3ba4e048ec22c02de311303f4a717d
Pascal Vizeli <pvizeli@syshack.ch>
2020-04-07 08:18:59 +0000

    fix lint

--- supervisor/api/supervisor.py
+++ supervisor/api/supervisor.py
@@ -43,7 +43,7 @@ from ..const import (
 from ..coresys import CoreSysAttributes
 from ..exceptions import APIError
 from ..utils.validate import validate_timezone
-from ..validate import log_level, repositories, wait_boot
+from ..validate import repositories, wait_boot
 from .utils import api_process, api_process_raw, api_validate
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
