ef9c32f97ff48dd139c586eae84805bbe0d969db
Pascal Vizeli <pvizeli@syshack.ch>
2020-04-22 08:29:18 +0200

    Fix handling with reset/default on json

--- supervisor/utils/json.py
+++ supervisor/utils/json.py
@@ -11,6 +11,7 @@ from ..exceptions import JsonFileError
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
 
+_DEFAULT: Dict[str, Any] = {}
 
 def write_json_file(jsonfile: Path, data: Any) -> None:
     """Write a JSON file."""
@@ -37,7 +38,7 @@ class JsonConfig:
         """Initialize hass object."""
         self._file: Path = json_file
         self._schema: vol.Schema = schema
-        self._data: Dict[str, Any] = {}
+        self._data: Dict[str, Any] = _DEFAULT
 
         self.read_data()
 
@@ -68,7 +69,7 @@ class JsonConfig:
 
             # Reset data to default
             _LOGGER.warning("Reset %s to default", self._file)
-            self._data = self._schema({})
+            self._data = self._schema(_DEFAULT)
 
     def save_data(self) -> None:
         """Store data to configuration file."""
@@ -80,6 +81,7 @@ class JsonConfig:
 
             # Load last valid data
             _LOGGER.warning("Reset %s to last version", self._file)
+            self._data = _DEFAULT
             self.read_data()
         else:
             # write
