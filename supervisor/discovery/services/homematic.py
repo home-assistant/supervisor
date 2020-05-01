4d094604eeaddad3e0873ba56b9c88cb6f342f80
Pascal Vizeli <pvizeli@syshack.ch>
2020-05-01 13:03:31 +0000

    Add support for homematic

---
+++ supervisor/discovery/services/homematic.py
@@ -0,0 +1,18 @@
+"""Discovery service for HomeMatic."""
+import voluptuous as vol
+
+from supervisor.validate import network_port
+
+from ..const import ATTR_HOST, ATTR_PORT
+
+
+SCHEMA = vol.Schema(
+    {
+        vol.Coerce(str): vol.Schema(
+            {
+                vol.Required(ATTR_HOST): vol.Coerce(str),
+                vol.Required(ATTR_PORT): network_port,
+            }
+        )
+    }
+)
