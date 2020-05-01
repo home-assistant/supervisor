9a2f025646db3c57c23c08239260eeadc9029240
Pascal Vizeli <pvizeli@syshack.ch>
2020-05-01 15:23:56 +0200

    Add support for homematic (#1690)

---
+++ tests/discovery/test_homematic.py
@@ -0,0 +1,22 @@
+"""Test HomeMatic discovery."""
+
+import voluptuous as vol
+import pytest
+
+from supervisor.discovery.validate import valid_discovery_config
+
+
+def test_good_config():
+    """Test good homematic config."""
+
+    valid_discovery_config(
+        "homematic",
+        {"ip": {"host": "test", "port": 3812}, "rf": {"host": "test", "port": 3712}},
+    )
+
+
+def test_bad_config():
+    """Test good homematic config."""
+
+    with pytest.raises(vol.Invalid):
+        valid_discovery_config("homematic", {"test": {"bla": "test", "port": 8080}})
