c605af6ccc45015e4543e3f26b35cd2afb2d7e75
Pascal Vizeli <pvizeli@syshack.ch>
2020-05-07 08:32:10 +0200

    Add discovery support for new ozw integration (#1701)

---
+++ supervisor/discovery/services/zwave_mqtt.py
@@ -0,0 +1,21 @@
+"""Discovery service for Zwave MQTT."""
+import voluptuous as vol
+
+from supervisor.validate import network_port
+
+from ..const import (
+    ATTR_HOST,
+    ATTR_PASSWORD,
+    ATTR_PORT,
+    ATTR_USERNAME,
+)
+
+# pylint: disable=no-value-for-parameter
+SCHEMA = vol.Schema(
+    {
+        vol.Required(ATTR_HOST): vol.Coerce(str),
+        vol.Required(ATTR_PORT): network_port,
+        vol.Required(ATTR_USERNAME): vol.Coerce(str),
+        vol.Required(ATTR_PASSWORD): vol.Coerce(str),
+    }
+)
