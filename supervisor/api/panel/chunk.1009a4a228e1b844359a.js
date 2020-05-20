026826d411058af841aa9b7c11fc03a90661d025
Pascal Vizeli <pvizeli@syshack.ch>
2020-05-20 15:33:10 +0000

    Update panel for markdown fixes
    Signed-off-by: Pascal Vizeli <pvizeli@syshack.ch>

---
+++ supervisor/api/panel/chunk.1009a4a228e1b844359a.js
@@ -0,0 +1,2 @@
+(self.webpackJsonp=self.webpackJsonp||[]).push([[1],{196:function(e,r,n){"use strict";n.r(r),n.d(r,"codeMirror",function(){return c}),n.d(r,"codeMirrorCss",function(){return i});var a=n(54),o=n.n(a),s=n(186),t=(n(187),n(188),n(12));o.a.commands.save=function(e){Object(t.a)(e.getWrapperElement(),"editor-save")};var c=o.a,i=s.a}}]);
+//# sourceMappingURL=chunk.1009a4a228e1b844359a.js.map
