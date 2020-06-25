fa65a38431f83c09f427d09e8839ca315959d7ef
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2020-06-25 11:56:45 +0200

    Add test for `_is_excluded_by_filter`
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- tests/utils/test_tarfile.py
+++ tests/utils/test_tarfile.py
@@ -2,7 +2,8 @@
 
 import attr
 
-from supervisor.utils.tar import secure_path
+from pathlib import PurePath
+from supervisor.utils.tar import secure_path, _is_excluded_by_filter
 
 
 @attr.s
@@ -31,3 +32,31 @@ def test_not_secure_path():
         TarInfo("/bla/blu/ble"),
     ]
     assert [] == list(secure_path(test_list))
+
+
+def test_is_excluded_by_filter_good():
+    """Test exclude filter."""
+    filter_list = ["not/match", "/dev/xy"]
+    test_list = [
+        PurePath("test.txt"),
+        PurePath("data/xy.blob"),
+        PurePath("bla/blu/ble"),
+        PurePath("data/../xy.blob"),
+    ]
+
+    for path_object in test_list:
+        assert _is_excluded_by_filter(path_object, filter_list) is False
+
+
+def test_is_exclude_by_filter_bad():
+    """Test exclude filter."""
+    filter_list = ["*.txt", "data/*", "bla/blu/ble"]
+    test_list = [
+        PurePath("test.txt"),
+        PurePath("data/xy.blob"),
+        PurePath("bla/blu/ble"),
+        PurePath("data/test_files/kk.txt"),
+    ]
+
+    for path_object in test_list:
+        assert _is_excluded_by_filter(path_object, filter_list) is True
