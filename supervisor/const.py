3ceec006acdec05d5e56c98ba2afa99571af8fa6
Pascal Vizeli <pvizeli@syshack.ch>
2020-08-12 14:47:09 +0000

    Fix isort

--- supervisor/const.py
+++ supervisor/const.py
@@ -3,7 +3,6 @@ from enum import Enum
 from ipaddress import ip_network
 from pathlib import Path
 
-
 SUPERVISOR_VERSION = "232"
 
 URL_HASSIO_ADDONS = "https://github.com/home-assistant/hassio-addons"
