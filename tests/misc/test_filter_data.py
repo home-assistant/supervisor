7f1af0d8a691a79a21b62f0d67de4b88bb86c0ef
Ludeeus <ludeeus@ludeeus.dev>
2020-08-15 11:57:53 +0000

    adjust test

--- tests/misc/test_filter_data.py
+++ tests/misc/test_filter_data.py
@@ -58,8 +58,8 @@ def test_defaults(coresys):
         filtered = filter_data(coresys, SAMPLE_EVENT, {})
 
     assert ["installation_type", "supervised"] in filtered["tags"]
-    assert filtered["extra"]["host"]["arch"] == "amd64"
-    assert filtered["extra"]["versions"]["supervisor"] == SUPERVISOR_VERSION
+    assert filtered["contexts"]["host"]["arch"] == "amd64"
+    assert filtered["contexts"]["versions"]["supervisor"] == SUPERVISOR_VERSION
 
 
 def test_sanitize(coresys):
