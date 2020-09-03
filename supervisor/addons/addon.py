58443b8a1a7c0b05b451d1660f3c9288747e190f
Pascal Vizeli <pvizeli@syshack.ch>
2020-09-03 14:10:27 +0000

    fix black

--- supervisor/addons/addon.py
+++ supervisor/addons/addon.py
@@ -187,7 +187,9 @@ class Addon(AddonModel):
     def watchdog(self, value: bool) -> None:
         """Set watchdog enable/disable."""
         if value and self.startup == AddonStartup.ONCE:
-            _LOGGER.warning("Ignoring watchdog for %s because startup type is 'once'", self.slug)
+            _LOGGER.warning(
+                "Ignoring watchdog for %s because startup type is 'once'", self.slug
+            )
         else:
             self.persist[ATTR_WATCHDOG] = value
 
