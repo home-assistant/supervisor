57fe1591feb6a1ad07eaf1b340dfe063f04173d1
Pascal Vizeli <pvizeli@syshack.ch>
2020-09-08 07:45:45 +0000

    fix tests

--- tests/test_core_state.py
+++ tests/test_core_state.py
@@ -1,15 +1,31 @@
 """Testing handling with CoreState."""
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from unittest.mock import patch
 
-from supervisor.const import RUN_SUPERVISOR_STATE, CoreState
+import pytest
+
+from supervisor.const import CoreState
 
 
-def test_write_state(coresys):
+@pytest.fixture
+def run_dir():
+    """Fixture to inject hassio env."""
+    with patch(
+        "supervisor.core.RUN_SUPERVISOR_STATE"
+    ) as mock_run, TemporaryDirectory() as tmp_run:
+        tmp_state = Path(tmp_run, "supervisor")
+        mock_run.write_text = tmp_state.write_text
+        yield tmp_state
+
+
+def test_write_state(run_dir, coresys):
     """Test write corestate to /run/supervisor."""
 
     coresys.core.state = CoreState.RUNNING
 
-    assert RUN_SUPERVISOR_STATE.read_text() == CoreState.RUNNING.value
+    assert run_dir.read_text() == CoreState.RUNNING.value
 
     coresys.core.state = CoreState.SHUTDOWN
 
-    assert RUN_SUPERVISOR_STATE.read_text() == CoreState.SHUTDOWN.value
+    assert run_dir.read_text() == CoreState.SHUTDOWN.value
