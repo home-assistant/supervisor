e4e516a6fe987ad24ba3d4c46c61e3bbfb3c3c34
Ludeeus <ludeeus@ludeeus.dev>
2020-09-14 09:54:53 +0000

    Capture TypeError in format

--- supervisor/utils/log_format.py
+++ supervisor/utils/log_format.py
@@ -1,15 +1,18 @@
 """Custom log messages."""
 import re
 
+import sentry_sdk
+
 RE_BIND_FAILED = re.compile(r".*Bind for.*:(\d*) failed: port is already allocated.*")
 
 
 def format_message(message: str) -> str:
     """Return a formated message if it's known."""
-    match = RE_BIND_FAILED.match(message)
-    if match:
-        return (
-            f"Port '{match.group(1)}' is already in use by something else on the host."
-        )
+    try:
+        match = RE_BIND_FAILED.match(message)
+        if match:
+            return f"Port '{match.group(1)}' is already in use by something else on the host."
+    except TypeError as err:
+        sentry_sdk.capture_exception(err)
 
     return message
