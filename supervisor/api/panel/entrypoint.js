f5fa892761b53175a15714f242be9dfaca5abecc
Ludeeus <ludeeus@ludeeus.dev>
2020-09-15 12:56:40 +0000

    Update panel to d6aba04

--- supervisor/api/panel/entrypoint.js
+++ supervisor/api/panel/entrypoint.js
@@ -1,9 +1,9 @@
 
 try {
-  new Function("import('/api/hassio/app/frontend_latest/entrypoint.babc4122.js')")();
+  new Function("import('/api/hassio/app/frontend_latest/entrypoint.ced77c9a.js')")();
 } catch (err) {
   var el = document.createElement('script');
-  el.src = '/api/hassio/app/frontend_es5/entrypoint.028a6bad.js';
+  el.src = '/api/hassio/app/frontend_es5/entrypoint.871af696.js';
   document.body.appendChild(el);
 }
   
