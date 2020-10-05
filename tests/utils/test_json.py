53f71bb783c938a023fc18c2ea61cf56d8a79455
Ludeeus <ludeeus@ludeeus.dev>
2020-10-05 12:02:10 +0000

    Use pytest fixture for tmp_path in tests

--- tests/utils/test_json.py
+++ tests/utils/test_json.py
@@ -1,25 +1,22 @@
 """test json."""
 from pathlib import Path
-from tempfile import TemporaryDirectory
 
 from supervisor.utils.json import write_json_file
 
 
-def test_file_permissions():
+def test_file_permissions(tmp_path):
     """Test file permissions."""
-    with TemporaryDirectory() as temp_dir:
-        tempfile = Path(temp_dir + "test.json")
-        write_json_file(tempfile, {"test": "data"})
-        assert tempfile.is_file()
-        assert oct(tempfile.stat().st_mode)[-3:] == "600"
+    tempfile = Path(tmp_path, "test.json")
+    write_json_file(tempfile, {"test": "data"})
+    assert tempfile.is_file()
+    assert oct(tempfile.stat().st_mode)[-3:] == "600"
 
 
-def test_new_file_permissions():
+def test_new_file_permissions(tmp_path):
     """Test file permissions."""
-    with TemporaryDirectory() as temp_dir:
-        tempfile = Path(temp_dir + "test.json")
-        tempfile.write_text("test")
-        assert oct(tempfile.stat().st_mode)[-3:] != "600"
+    tempfile = Path(tmp_path, "test.json")
+    tempfile.write_text("test")
+    assert oct(tempfile.stat().st_mode)[-3:] != "600"
 
-        write_json_file(tempfile, {"test": "data"})
-        assert oct(tempfile.stat().st_mode)[-3:] == "600"
+    write_json_file(tempfile, {"test": "data"})
+    assert oct(tempfile.stat().st_mode)[-3:] == "600"
