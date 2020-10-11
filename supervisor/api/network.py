b8b700a953dc445204945418feba4cd75a8b716b
Pascal Vizeli <pvizeli@syshack.ch>
2020-10-11 10:28:03 +0000

    add interface to info

--- supervisor/api/network.py
+++ supervisor/api/network.py
@@ -39,6 +39,7 @@ SCHEMA_UPDATE = vol.Schema(
 def interface_information(interface: NetworkInterface) -> dict:
     """Return a dict with information of a interface to be used in th API."""
     return {
+        ATTR_INTERFACE: interface.name,
         ATTR_IP_ADDRESS: f"{interface.ip_address}/{interface.prefix}",
         ATTR_GATEWAY: interface.gateway,
         ATTR_ID: interface.id,
