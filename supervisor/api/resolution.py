55036300be936ebe8b3dcce7afd6a672e31f57c6
Ludeeus <ludeeus@ludeeus.dev>
2020-10-14 11:41:41 +0000

    Add apply suggestion to API

--- supervisor/api/resolution.py
+++ supervisor/api/resolution.py
@@ -5,6 +5,8 @@ from aiohttp import web
 
 from ..const import ATTR_ISSUES, ATTR_SUGGESTIONS, ATTR_UNSUPPORTED
 from ..coresys import CoreSysAttributes
+from ..exceptions import APIError
+from ..resolution.const import Suggestion
 from .utils import api_process
 
 
@@ -12,10 +14,19 @@ class APIResoulution(CoreSysAttributes):
     """Handle REST API for resoulution."""
 
     @api_process
-    async def base(self, request: web.Request) -> Dict[str, Any]:
+    async def base(self, _request: web.Request) -> Dict[str, Any]:
         """Return network information."""
         return {
             ATTR_UNSUPPORTED: self.sys_resolution.unsupported,
             ATTR_SUGGESTIONS: self.sys_resolution.suggestions,
             ATTR_ISSUES: self.sys_resolution.issues,
         }
+
+    @api_process
+    async def suggestion(self, request: web.Request) -> None:
+        """Apply suggestion."""
+        try:
+            suggestion = Suggestion(request.match_info.get("suggestion"))
+            await self.sys_resolution.apply_suggestion(suggestion)
+        except ValueError:
+            raise APIError("Suggestion is not valid") from None
