1c1dbcd581a39cbf8331f0a0330519de47a6c05f
Pascal Vizeli <pvizeli@syshack.ch>
2020-10-20 15:37:27 +0000

    fix version

--- scripts/dev_version.sh
+++ scripts/dev_version.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -eE
 
-setup_version="$(python setup.py -V)"
 commit_count="$(git rev-list --count --since=yesterday HEAD)"
-commit_date="$(date "+%d")"
+calver_date="$(date "+%Y.%m.dev%d")"
 
-commit_version="${setup_version}.dev${commit_date}${commit_count}"
+calver_version="${setup_version}.dev${calver_date}${commit_count}"
 
-sed -i "s/SUPERVISOR_VERSION .*/SUPERVISOR_VERSION = \"${commit_version}\"/g" supervisor/const.py
-echo "$commit_version"
+sed -i "s/SUPERVISOR_VERSION .*/SUPERVISOR_VERSION = \"${calver_version}\"/g" supervisor/const.py
+echo "$calver_version"
