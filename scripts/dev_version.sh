a98c7819b0252f6b3eb5d84ce28e888eeb1aebf5
Pascal Vizeli <pvizeli@syshack.ch>
2020-10-21 12:02:43 +0000

    Fix version script

--- scripts/dev_version.sh
+++ scripts/dev_version.sh
@@ -4,7 +4,7 @@ set -eE
 commit_count="$(git rev-list --count --since=yesterday HEAD)"
 calver_date="$(date "+%Y.%m.dev%d")"
 
-calver_version="${setup_version}.dev${calver_date}${commit_count}"
+calver_version="${calver_date}${commit_count}"
 
 sed -i "s/SUPERVISOR_VERSION .*/SUPERVISOR_VERSION = \"${calver_version}\"/g" supervisor/const.py
 echo "$calver_version"
