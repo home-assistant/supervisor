aa7b68d4d5da14a1f51c0c42be324a7771b766b4
Joakim Sørensen <joasoe@gmail.com>
2020-10-26 21:14:42 +0100

    Fix missing await (#2177)
    * Fix missing await
    
    * One more

--- supervisor/store/git.py
+++ supervisor/store/git.py
@@ -53,7 +53,7 @@ class GitRepo(CoreSysAttributes):
                 git.GitCommandError,
             ) as err:
                 _LOGGER.error("Can't load %s repo: %s.", self.path, err)
-                self._remove()
+                await self._remove()
                 return False
 
             return True
@@ -86,7 +86,7 @@ class GitRepo(CoreSysAttributes):
                 git.GitCommandError,
             ) as err:
                 _LOGGER.error("Can't clone %s repository: %s.", self.url, err)
-                self._remove()
+                await self._remove()
                 return False
 
             return True
