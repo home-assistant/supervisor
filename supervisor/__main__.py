28344ff5f35bb4e0fa18f7b432be8665fd8afe3a
Pascal Vizeli <pvizeli@syshack.ch>
2020-11-03 16:36:10 +0100

    Supervisor container startup health function (#2214)
    * Supervisor container startup health function
    
    * better struct
    
    * add test
    
    * address comment
    
    * rename file
    
    * Update tests/test_main.py
    
    Co-authored-by: Stefan Agner <stefan@agner.ch>
    
    Co-authored-by: Stefan Agner <stefan@agner.ch>

--- supervisor/__main__.py
+++ supervisor/__main__.py
@@ -2,11 +2,25 @@
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
 import logging
+from pathlib import Path
 import sys
 
 from supervisor import bootstrap
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
+
+CONTAINER_OS_STARTUP_CHECK = Path("/run/os/startup-marker")
+
+
+def run_os_startup_check_cleanup() -> None:
+    """Cleanup OS startup check."""
+    if not CONTAINER_OS_STARTUP_CHECK.exists():
+        return
+
+    try:
+        CONTAINER_OS_STARTUP_CHECK.unlink()
+    except OSError as err:
+        _LOGGER.warning("Not able to remove the startup health file: %s", err)
 
 
 # pylint: disable=invalid-name
@@ -29,6 +43,9 @@ if __name__ == "__main__":
 
     bootstrap.supervisor_debugger(coresys)
     bootstrap.migrate_system_env(coresys)
+
+    # Signal health startup for container
+    run_os_startup_check_cleanup()
 
     _LOGGER.info("Setting up Supervisor")
     loop.run_until_complete(coresys.core.setup())
