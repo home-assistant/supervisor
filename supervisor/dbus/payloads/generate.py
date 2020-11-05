5c13abc079e66a9600ed228b9f6c7209b2edbb04
Pascal Vizeli <pvizeli@syshack.ch>
2020-11-05 17:16:00 +0000

    fix typing import

--- supervisor/dbus/payloads/generate.py
+++ supervisor/dbus/payloads/generate.py
@@ -1,10 +1,14 @@
 """Payload generators for DBUS communication."""
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 import jinja2
 
 from ...host.const import AuthMethod
-from ...host.network import Interface
+
+if TYPE_CHECKING:
+    from ...host.network import Interface
+
 
 INTERFACE_UPDATE_TEMPLATE: Path = (
     Path(__file__).parents[2].joinpath("dbus/payloads/interface_update.tmpl")
