86e5a69b2996f1ba720e653dabbc54c933078a07
Ludeeus <ludeeus@ludeeus.dev>
2020-11-05 09:28:01 +0000

    Create new timestamp if OverflowError

--- supervisor/ingress.py
+++ supervisor/ingress.py
@@ -116,8 +116,8 @@ class Ingress(JsonConfig, CoreSysAttributes):
         try:
             valid_until = utc_from_timestamp(self.sessions[session])
         except OverflowError:
-            _LOGGER.warning("Session timestamp %f is invalid!", self.sessions[session])
-            return False
+            self.sessions[session] = utcnow() + timedelta(minutes=15)
+            return True
 
         # Is still valid?
         if valid_until < utcnow():
