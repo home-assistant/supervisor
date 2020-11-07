f345636efd6d7205a32440de6658803bb2da5960
Pascal Vizeli <pvizeli@syshack.ch>
2020-11-07 13:14:45 +0000

    refactory p8
    Signed-off-by: Pascal Vizeli <pvizeli@syshack.ch>

--- supervisor/dbus/network/wireless.py
+++ supervisor/dbus/network/wireless.py
@@ -1,23 +1,16 @@
 """Connection object for Network Manager."""
-from typing import Any, Awaitable, Dict, Optional
+from typing import Any, Awaitable, Optional
 
 from ...utils.gdbus import DBus
 from ..const import (
     DBUS_ATTR_ACTIVE_ACCESSPOINT,
-    DBUS_ATTR_CONNECTION,
-    DBUS_ATTR_FREQUENCY,
-    DBUS_ATTR_HWADDRESS,
-    DBUS_ATTR_MODE,
-    DBUS_ATTR_SSID,
-    DBUS_ATTR_STRENGTH,
-    DBUS_NAME_ACCESSPOINT,
     DBUS_NAME_DEVICE_WIRELESS,
     DBUS_NAME_NM,
     DBUS_OBJECT_BASE,
 )
 from ..interface import DBusInterfaceProxy
 from ..utils import dbus_connected
-from .configuration import APConfiguration
+from .accesspoint import NetworkWirelessAP
 
 
 class NetworkWireless(DBusInterfaceProxy):
@@ -28,17 +21,12 @@ class NetworkWireless(DBusInterfaceProxy):
         self.object_path = object_path
         self.properties = {}
 
-        self._active: Optional[APConfiguration] = None
+        self._active: Optional[NetworkWirelessAP] = None
 
     @property
-    def active(self) -> Optional[APConfiguration]:
+    def active(self) -> Optional[NetworkWirelessAP]:
         """Return details about active connection."""
         return self._active
-
-    @property
-    def setting_object(self) -> int:
-        """Return the connection object path."""
-        return self.properties[DBUS_ATTR_CONNECTION]
 
     @dbus_connected
     def request_scan(self) -> Awaitable[None]:
@@ -50,19 +38,6 @@ class NetworkWireless(DBusInterfaceProxy):
         """Return a list of all access points path."""
         return self.dbus.Device.Wireless.GetAllAccessPoints()
 
-    async def object_to_accesspoints(self, object_path: str) -> APConfiguration:
-        """Convert an AP object path into object."""
-        ap = DBus.connect(DBUS_NAME_NM, object_path)
-        data = ap.get_properties(DBUS_NAME_ACCESSPOINT)
-
-        return APConfiguration(
-            bytes(data[DBUS_ATTR_SSID]).decode(),
-            data[DBUS_ATTR_FREQUENCY],
-            data[DBUS_ATTR_HWADDRESS],
-            data[DBUS_ATTR_MODE],
-            int(bytes(data[DBUS_ATTR_STRENGTH])),
-        )
-
     async def connect(self) -> None:
         """Get connection information."""
         self.dbus = await DBus.connect(DBUS_NAME_NM, self.object_path)
@@ -70,6 +45,7 @@ class NetworkWireless(DBusInterfaceProxy):
 
         # Get details from current active
         if self.properties[DBUS_ATTR_ACTIVE_ACCESSPOINT] != DBUS_OBJECT_BASE:
-            self._active = await self.object_to_accesspoints(
+            self._active = NetworkWirelessAP(
                 self.properties[DBUS_ATTR_ACTIVE_ACCESSPOINT]
             )
+            await self._active.connect()
