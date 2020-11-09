4c560d7c5401372a8cb800dca99afab194bed26d
Pascal Vizeli <pvizeli@syshack.ch>
2020-11-09 10:00:39 +0100

    Fix gateway None on API (#2230)
    * Fix gateway None on API
    
    * fix tests

--- supervisor/api/network.py
+++ supervisor/api/network.py
@@ -84,7 +84,7 @@ def ipconfig_struct(config: IpConfig) -> dict:
         ATTR_METHOD: config.method,
         ATTR_ADDRESS: [address.with_prefixlen for address in config.address],
         ATTR_NAMESERVERS: [str(address) for address in config.nameservers],
-        ATTR_GATEWAY: str(config.gateway),
+        ATTR_GATEWAY: str(config.gateway) if config.gateway else None,
     }
 
 
