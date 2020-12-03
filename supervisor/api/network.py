5d4f1d936ef329009424709c8710f6346092f2d9
Ludeeus <ludeeus@ludeeus.dev>
2020-12-03 20:13:44 +0000

    Return VLAN ID instead of wifistruct for VLAN

--- supervisor/api/network.py
+++ supervisor/api/network.py
@@ -111,7 +111,7 @@ def interface_struct(interface: Interface) -> dict:
         ATTR_IPV4: ipconfig_struct(interface.ipv4) if interface.ipv4 else None,
         ATTR_IPV6: ipconfig_struct(interface.ipv6) if interface.ipv6 else None,
         ATTR_WIFI: wifi_struct(interface.wifi) if interface.wifi else None,
-        ATTR_VLAN: wifi_struct(interface.vlan) if interface.vlan else None,
+        ATTR_VLAN: interface.vlan.id if interface.vlan else None,
     }
 
 
