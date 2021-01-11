1434077f4e892c0c92bd412c8e52e698685e9c1a
Joakim Sørensen <joasoe@gmail.com>
2021-01-11 15:22:10 +0100

    Update frontend panel to edcb7e87 (#2422)

--- supervisor/api/panel/entrypoint.js
+++ supervisor/api/panel/entrypoint.js
@@ -1,9 +1,9 @@
 
 try {
-  new Function("import('/api/hassio/app/frontend_latest/entrypoint.df099659.js')")();
+  new Function("import('/api/hassio/app/frontend_latest/entrypoint.266bc30f.js')")();
 } catch (err) {
   var el = document.createElement('script');
-  el.src = '/api/hassio/app/frontend_es5/entrypoint.d303236e.js';
+  el.src = '/api/hassio/app/frontend_es5/entrypoint.585f219b.js';
   document.body.appendChild(el);
 }
   
