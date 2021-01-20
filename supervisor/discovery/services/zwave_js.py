df8667e384e9cf5a85337fd85bcc9c9292025ffc
Pascal Vizeli <pvizeli@syshack.ch>
2021-01-20 15:32:57 +0000

    Add discovery support for zwave_js

--- supervisor/discovery/services/zwave_js.py
+++ supervisor/discovery/services/zwave_js.py
@@ -1,16 +1,14 @@
-"""Discovery service for OpenZwave MQTT."""
+"""Discovery service for Zwave JS."""
 import voluptuous as vol
 
 from supervisor.validate import network_port
 
-from ..const import ATTR_HOST, ATTR_PASSWORD, ATTR_PORT, ATTR_USERNAME
+from ..const import ATTR_HOST, ATTR_PORT
 
 # pylint: disable=no-value-for-parameter
 SCHEMA = vol.Schema(
     {
         vol.Required(ATTR_HOST): vol.Coerce(str),
         vol.Required(ATTR_PORT): network_port,
-        vol.Required(ATTR_USERNAME): vol.Coerce(str),
-        vol.Required(ATTR_PASSWORD): vol.Coerce(str),
     }
 )
