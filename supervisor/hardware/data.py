c54d536edecac15bea7e70da19618586fb9099c3
Pascal Vizeli <pvizeli@syshack.ch>
2021-01-27 20:43:06 +0000

    add helper for by-id

--- supervisor/hardware/data.py
+++ supervisor/hardware/data.py
@@ -1,6 +1,6 @@
 """Data representation of Hardware."""
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 import attr
 
@@ -19,9 +19,18 @@ class Device:
     @property
     def cgroups_major(self) -> int:
         """Return Major cgroups."""
-        return self.attributes.get("MAJOR", 0)
+        return int(self.attributes.get("MAJOR", 0))
 
     @property
     def cgroups_minor(self) -> int:
         """Return Major cgroups."""
-        return self.attributes.get("MINOR", 0)
+        return int(self.attributes.get("MINOR", 0))
+
+    @property
+    def by_id(self) -> Optional[Path]:
+        """Return path by-id."""
+        for link in self.links:
+            if not link.match("/dev/*/by-id/*"):
+                continue
+            return link
+        return None
