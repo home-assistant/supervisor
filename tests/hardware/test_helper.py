cd4d16588f9517124a2fc78f1dc86fae91c2a1a5
Pascal Vizeli <pvizeli@syshack.ch>
2021-01-31 13:35:05 +0000

    fix test

--- tests/hardware/test_helper.py
+++ tests/hardware/test_helper.py
@@ -72,7 +72,7 @@ def test_hide_virtual_device(coresys):
     udev_device.sys_path = "/sys/devices/virtual/tty/tty1"
     assert coresys.hardware.helper.hide_virtual_device(udev_device)
 
-    udev_device.sys_path = "/sys/devices/virtual/vs/vcs1"
+    udev_device.sys_path = "/sys/devices/virtual/vc/vcs1"
     assert coresys.hardware.helper.hide_virtual_device(udev_device)
 
 
