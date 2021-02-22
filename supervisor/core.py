3e3c905aa537e51f896689be0682e35d7d3336fb
Ludeeus <ludeeus@ludeeus.dev>
2021-02-22 12:32:40 +0000

    Signal frontend to refresh on SU startup

--- supervisor/core.py
+++ supervisor/core.py
@@ -244,6 +244,9 @@ class Core(CoreSysAttributes):
             self.sys_create_task(self.sys_resolution.fixup.run_autofix())
 
             self.state = CoreState.RUNNING
+            await self.sys_homeassistant.websocket.async_supervisor_update_event(
+                "supervisor", {}
+            )
             _LOGGER.info("Supervisor is up and running")
 
     async def stop(self):
