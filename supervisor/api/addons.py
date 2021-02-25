ae3274e559b78ac30051adb32c662f17ffb10220
Joakim Sørensen <joasoe@gmail.com>
2021-02-25 11:44:22 +0100

    Add home-assistant version to API response (#2621)

--- supervisor/api/addons.py
+++ supervisor/api/addons.py
@@ -171,6 +171,7 @@ class APIAddons(CoreSysAttributes):
                 ATTR_INSTALLED: addon.is_installed,
                 ATTR_AVAILABLE: addon.available,
                 ATTR_DETACHED: addon.is_detached,
+                ATTR_HOMEASSISTANT: addon.homeassistant_version,
                 ATTR_REPOSITORY: addon.repository,
                 ATTR_BUILD: addon.need_build,
                 ATTR_URL: addon.url,
