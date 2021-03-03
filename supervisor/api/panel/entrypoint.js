202e94615e7c05d7da91549c5ca190b1293fa55f
Joakim Sørensen <joasoe@gmail.com>
2021-03-03 15:15:07 +0100

    Update frontend to bee17fce (#2665)
    * Update frontend to fe50f422
    
    * revert unrelated change
    
    * Update frontend to bee17fce

--- supervisor/api/panel/entrypoint.js
+++ supervisor/api/panel/entrypoint.js
@@ -1,9 +1,9 @@
 
 try {
-  new Function("import('/api/hassio/app/frontend_latest/entrypoint.b954f17a.js')")();
+  new Function("import('/api/hassio/app/frontend_latest/entrypoint.fae75f85.js')")();
 } catch (err) {
   var el = document.createElement('script');
-  el.src = '/api/hassio/app/frontend_es5/entrypoint.f1b08733.js';
+  el.src = '/api/hassio/app/frontend_es5/entrypoint.358c173a.js';
   document.body.appendChild(el);
 }
   
