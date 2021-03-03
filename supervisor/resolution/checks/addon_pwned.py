29a0377017105666f3e5b11fb44e6f9888bc8e9a
Ludeeus <ludeeus@ludeeus.dev>
2021-03-03 10:17:40 +0000

    revert unrelated change

--- supervisor/resolution/checks/addon_pwned.py
+++ supervisor/resolution/checks/addon_pwned.py
@@ -16,7 +16,7 @@ class CheckAddonPwned(CheckBase):
     """CheckAddonPwned class for check."""
 
     @Job(
-        conditions=[JobCondition.INTERNET_SYSTEM, JobCondition.HEALTHY],
+        conditions=[JobCondition.INTERNET_SYSTEM],
         limit=JobExecutionLimit.THROTTLE,
         throttle_period=timedelta(hours=24),
     )
