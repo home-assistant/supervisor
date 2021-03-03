2f29c3bcc9601db6de16759085067086dd8580e6
Ludeeus <ludeeus@ludeeus.dev>
2021-03-03 09:59:23 +0000

    Guard for missing repository file

--- supervisor/store/data.py
+++ supervisor/store/data.py
@@ -65,6 +65,9 @@ class StoreData(CoreSysAttributes):
             path, "repository", FILE_SUFFIX_CONFIGURATION
         )
 
+        if repository_file is None:
+            return
+
         try:
             repository_info = SCHEMA_REPOSITORY_CONFIG(
                 read_json_or_yaml_file(repository_file)
