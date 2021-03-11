cc9153fa4118f23a45ccacd228d37e88052cc3b1
Pascal Vizeli <pvizeli@syshack.ch>
2021-03-11 11:13:53 +0000

    Cache hits on pwned

--- supervisor/utils/pwned.py
+++ supervisor/utils/pwned.py
@@ -10,10 +10,15 @@ from ..exceptions import PwnedConnectivityError, PwnedError
 _LOGGER: logging.Logger = logging.getLogger(__name__)
 _API_CALL = "https://api.pwnedpasswords.com/range/{hash}"
 
+_CACHE = set()
+
 
 async def check_pwned_password(websession: aiohttp.ClientSession, sha1_pw: str) -> bool:
     """Check if password is pwned."""
     sha1_pw = sha1_pw.upper()
+    if sha1_pw[:5] in _CACHE:
+        return True
+
     try:
         async with websession.get(
             _API_CALL.format(hash=sha1_pw[:5]), timeout=aiohttp.ClientTimeout(total=10)
@@ -26,6 +31,7 @@ async def check_pwned_password(websession: aiohttp.ClientSession, sha1_pw: str) 
         for line in buffer:
             if not sha1_pw.endswith(line.split(":")[0]):
                 continue
+            _CACHE.add(sha1_pw[:5])
             return True
 
     except (aiohttp.ClientError, asyncio.TimeoutError) as err:
