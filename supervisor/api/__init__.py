55382d000bb6dff90eca6366a92a27ad85873b74
Pascal Vizeli <pvizeli@syshack.ch>
2021-03-15 10:16:32 +0100

    Revert "Tweak check API path" (#2718)
    This reverts commit e30171746bb6170fa0a8897b73c54baf4844c740.

--- supervisor/api/__init__.py
+++ supervisor/api/__init__.py
@@ -228,7 +228,9 @@ class RestAPI(CoreSysAttributes):
         self.webapp.add_routes(
             [
                 web.get("/resolution/info", api_resolution.info),
-                web.post("/resolution/check/{check}", api_resolution.options_check),
+                web.post(
+                    "/resolution/check/{check}/options", api_resolution.options_check
+                ),
                 web.post(
                     "/resolution/suggestion/{suggestion}",
                     api_resolution.apply_suggestion,
