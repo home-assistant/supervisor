538b4bf71e9cc0ff9632a6c036777112ba9a3e28
Pascal Vizeli <pvizeli@syshack.ch>
2021-04-28 09:59:18 +0000

    Fix WSL2 issues + cleanup scripts

--- scripts/supervisor.sh
+++ scripts/supervisor.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 source "${BASH_SOURCE[0]%/*}/common.sh"
-source "${BASH_SOURCE[0]%/*}/build-supervisor.sh"
 
 set -eE
 
 DOCKER_TIMEOUT=30
 DOCKER_PID=0
+
+function build_supervisor() {
+    docker pull homeassistant/amd64-builder:dev
+
+    docker run --rm \
+        --privileged \
+        -v /run/docker.sock:/run/docker.sock \
+        -v "$(pwd):/data" \
+        homeassistant/amd64-builder:dev \
+            --generic latest \
+            --target /data \
+            --test \
+            --amd64 \
+            --no-cache
+}
 
 
 function cleanup_docker() {
