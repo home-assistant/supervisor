40e8e9b37ef869be7f0eea80599c4d757fa0bf25
Pascal Vizeli <pvizeli@syshack.ch>
2021-05-19 11:53:46 +0000

    better testing

--- tests/dbus/test_timedate.py
+++ tests/dbus/test_timedate.py
@@ -1,7 +1,10 @@
 """Test TimeDate dbus interface."""
 from datetime import datetime, timezone
 
+import pytest
+
 from supervisor.coresys import CoreSys
+from supervisor.exceptions import DBusNotConnectedError
 
 
 async def test_dbus_timezone(coresys: CoreSys):
@@ -20,14 +23,21 @@ async def test_dbus_timezone(coresys: CoreSys):
 
 async def test_dbus_settime(coresys: CoreSys):
     """Set timestamp on backend."""
+    test_dt = datetime(2021, 5, 19, 8, 36, 54, 405718, tzinfo=timezone.utc)
+
+    with pytest.raises(DBusNotConnectedError):
+        await coresys.dbus.timedate.set_time(test_dt)
+
     await coresys.dbus.timedate.connect()
 
-    dt = datetime(2021, 5, 19, 8, 36, 54, 405718, tzinfo=timezone.utc)
-    await coresys.dbus.timedate.set_time(dt)
+    await coresys.dbus.timedate.set_time(test_dt)
 
 
 async def test_dbus_setntp(coresys: CoreSys):
     """Disable NTP on backend."""
+    with pytest.raises(DBusNotConnectedError):
+        await coresys.dbus.timedate.set_ntp(False)
+
     await coresys.dbus.timedate.connect()
 
     await coresys.dbus.timedate.set_ntp(False)
