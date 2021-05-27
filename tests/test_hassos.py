67dbf6beeca725775457d312f9ceb6671be6712d
Stefan Agner <stefan@agner.ch>
2021-05-27 14:14:41 +0200

    Update tests/test_hassos.py
    Co-authored-by: Pascal Vizeli <pascal.vizeli@syshack.ch>

--- tests/test_hassos.py
+++ tests/test_hassos.py
@@ -16,10 +16,9 @@ async def test_ota_url_generic_x86_64_rename(coresys: CoreSys) -> None:
     coresys.hassos._version = AwesomeVersion("5.13")
     await coresys.updater.fetch_data()
 
-    version6str = "6.0"
-    version6 = AwesomeVersion(version6str)
+    version6 = AwesomeVersion("6.0")
     url = coresys.updater.ota_url.format(
-        version=str(version6str), board="generic-x86-64"
+        version=str(version6), board="generic-x86-64"
     )
 
     assert coresys.hassos._get_download_url(version6) == url
