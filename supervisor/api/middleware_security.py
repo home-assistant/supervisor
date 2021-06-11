008ddf16e1c4493ab114dbd2022ba78d8052a2d1
Pascal Vizeli <pvizeli@syshack.ch>
2021-06-11 13:01:26 +0000

    fix check

--- supervisor/api/middleware_security.py
+++ supervisor/api/middleware_security.py
@@ -53,7 +53,7 @@ ADDONS_API_BYPASS = re.compile(
     r"|/addons/self/(?!security|update)[^/]+"
     r"|/addons/self/options/config"
     r"|/info"
-    r"|/network/.+/info"
+    r"|/network/interface/[^/]+/info"
     r"|/services.*"
     r"|/discovery.*"
     r"|/auth"
