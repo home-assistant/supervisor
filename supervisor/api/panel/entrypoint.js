e80652adc224072e0724154e8162fc73d1c78825
Joakim Sørensen <ludeeus@ludeeus.dev>
2021-06-16 09:42:19 +0000

    Update frontend to 446a9b5c

--- supervisor/api/panel/entrypoint.js
+++ supervisor/api/panel/entrypoint.js
@@ -1,9 +1,9 @@
 
 try {
-  new Function("import('/api/hassio/app/frontend_latest/entrypoint.586ea840.js')")();
+  new Function("import('/api/hassio/app/frontend_latest/entrypoint.0d3c68f7.js')")();
 } catch (err) {
   var el = document.createElement('script');
-  el.src = '/api/hassio/app/frontend_es5/entrypoint.8daaaeda.js';
+  el.src = '/api/hassio/app/frontend_es5/entrypoint.9e377d5a.js';
   document.body.appendChild(el);
 }
   
