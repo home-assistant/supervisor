d652f82c7f55975be378332365385d4716ed27ce
Joakim Sørensen <ludeeus@ludeeus.dev>
2021-06-25 14:48:30 +0000

    Fix failing test

--- tests/test_hassos.py
+++ tests/test_hassos.py
@@ -11,13 +11,14 @@ from supervisor.coresys import CoreSys
 @pytest.mark.asyncio
 async def test_ota_url_generic_x86_64_rename(coresys: CoreSys) -> None:
     """Test download URL generated."""
-
     coresys.hassos._board = "intel-nuc"
     coresys.hassos._version = AwesomeVersion("5.13")
     await coresys.updater.fetch_data()
 
     version6 = AwesomeVersion("6.0")
-    url = coresys.updater.ota_url.format(version=str(version6), board="generic-x86-64")
+    url = coresys.updater.ota_url.format(
+        version=str(version6), board="generic-x86-64", os_name="haos"
+    )
 
     assert coresys.hassos._get_download_url(version6) == url
 
