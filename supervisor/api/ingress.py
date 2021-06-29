011b6965b37abb8c66f65846f9f39df195edcbd1
Pascal Vizeli <pvizeli@syshack.ch>
2021-06-29 10:21:34 +0200

    Update supervisor/api/ingress.py
    Co-authored-by: Joakim Sørensen <joasoe@gmail.com>

--- supervisor/api/ingress.py
+++ supervisor/api/ingress.py
@@ -164,6 +164,9 @@ class APIIngress(CoreSysAttributes):
         url = self._create_url(addon, path)
         source_header = _init_header(request, addon)
 
+        # Passing the raw stream breaks requests for some webservers 
+        # since we just need it for POST requests really, for all other methods 
+        # we read the bytes and pass that to the request to the add-on
         data = request.content if request.method == "POST" else await request.read()
 
         async with self.sys_websession.request(
