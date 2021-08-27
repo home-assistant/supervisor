c472fc572594f82a5ba2a7ee4d3d2a665ee7f491
Ludeeus <ludeeus@ludeeus.dev>
2021-08-27 15:25:55 +0000

    Update frontend to 67ab63f0

--- supervisor/api/panel/entrypoint.js
+++ supervisor/api/panel/entrypoint.js
@@ -1,14 +1,14 @@
 
 function loadES5() {
   var el = document.createElement('script');
-  el.src = '/api/hassio/app/frontend_es5/entrypoint.ef32933f.js';
+  el.src = '/api/hassio/app/frontend_es5/entrypoint.e3a9299f.js';
   document.body.appendChild(el);
 }
 if (/.*Version\/(?:11|12)(?:\.\d+)*.*Safari\//.test(navigator.userAgent)) {
     loadES5();
 } else {
   try {
-    new Function("import('/api/hassio/app/frontend_latest/entrypoint.7ca7c83a.js')")();
+    new Function("import('/api/hassio/app/frontend_latest/entrypoint.48b24b78.js')")();
   } catch (err) {
     loadES5();
   }
