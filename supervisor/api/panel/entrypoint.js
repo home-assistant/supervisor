6df7a88666b087f3b8fde3f76095efa080b65c99
Joakim Sørensen <joasoe@gmail.com>
2021-09-25 10:59:53 +0200

    Bump frontend to 03dc3e52 (#3142)

--- supervisor/api/panel/entrypoint.js
+++ supervisor/api/panel/entrypoint.js
@@ -1,14 +1,14 @@
 
 function loadES5() {
   var el = document.createElement('script');
-  el.src = '/api/hassio/app/frontend_es5/entrypoint.1796fc9b.js';
+  el.src = '/api/hassio/app/frontend_es5/entrypoint.fcfbe018.js';
   document.body.appendChild(el);
 }
 if (/.*Version\/(?:11|12)(?:\.\d+)*.*Safari\//.test(navigator.userAgent)) {
     loadES5();
 } else {
   try {
-    new Function("import('/api/hassio/app/frontend_latest/entrypoint.5a9040a4.js')")();
+    new Function("import('/api/hassio/app/frontend_latest/entrypoint.4a3f19b4.js')")();
   } catch (err) {
     loadES5();
   }
