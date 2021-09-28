d76f4e158960e6fab0ac2363d35c3fb844cab382
Ludeeus <ludeeus@ludeeus.dev>
2021-09-28 07:24:16 +0000

    Bmp frontend to b6344eb6

--- supervisor/api/panel/entrypoint.js
+++ supervisor/api/panel/entrypoint.js
@@ -1,14 +1,14 @@
 
 function loadES5() {
   var el = document.createElement('script');
-  el.src = '/api/hassio/app/frontend_es5/entrypoint.fcfbe018.js';
+  el.src = '/api/hassio/app/frontend_es5/entrypoint.beb8aae2.js';
   document.body.appendChild(el);
 }
 if (/.*Version\/(?:11|12)(?:\.\d+)*.*Safari\//.test(navigator.userAgent)) {
     loadES5();
 } else {
   try {
-    new Function("import('/api/hassio/app/frontend_latest/entrypoint.4a3f19b4.js')")();
+    new Function("import('/api/hassio/app/frontend_latest/entrypoint.b658878e.js')")();
   } catch (err) {
     loadES5();
   }
