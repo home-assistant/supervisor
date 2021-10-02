5e9fb2fc70781873938332f6552c01d1c2f9c852
vighneshkadam007 <vighneshkadamdc@gmail.com>
2021-10-02 22:08:10 +0530

    Use new format for logging exceptions-plugins/cli.py

--- supervisor/plugins/cli.py
+++ supervisor/plugins/cli.py
@@ -52,7 +52,9 @@ class PluginCli(PluginBase):
 
             await self.instance.attach(version=self.version)
         except DockerError:
-            _LOGGER.info("No cli plugin Docker image %s found.", self.instance.image)
+            raise DockerError(
+                f"No cli plugin Docker image {self.instance.image} found.", _LOGGER.info
+            )
 
             # Install cli
             with suppress(CliError):
@@ -102,8 +104,7 @@ class PluginCli(PluginBase):
         try:
             await self.instance.update(version, image=self.sys_updater.image_cli)
         except DockerError as err:
-            _LOGGER.error("CLI update failed")
-            raise CliUpdateError() from err
+            raise CliUpdateError("CLI update failed", _LOGGER.error) from err
         else:
             self.version = version
             self.image = self.sys_updater.image_cli
@@ -127,8 +128,7 @@ class PluginCli(PluginBase):
         try:
             await self.instance.run()
         except DockerError as err:
-            _LOGGER.error("Can't start cli plugin")
-            raise CliError() from err
+            raise CliError("Can't start cli plugin", _LOGGER.error) from err
 
     async def stop(self) -> None:
         """Stop cli."""
@@ -136,8 +136,7 @@ class PluginCli(PluginBase):
         try:
             await self.instance.stop()
         except DockerError as err:
-            _LOGGER.error("Can't stop cli plugin")
-            raise CliError() from err
+            raise CliError("Can't stop cli plugin", _LOGGER.error) from err
 
     async def stats(self) -> DockerStats:
         """Return stats of cli."""
