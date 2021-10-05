2ef5f25a666f2373fc2e53827bdd188b5c2d0dec
Ludeeus <ludeeus@ludeeus.dev>
2021-10-05 07:55:12 +0000

    Set agent diagnostics

--- supervisor/dbus/agent/__init__.py
+++ supervisor/dbus/agent/__init__.py
@@ -70,6 +70,7 @@ class OSAgent(DBusInterface):
         return self.properties[DBUS_ATTR_DIAGNOSTICS]
 
     @diagnostics.setter
+    @dbus_connected
     def diagnostics(self, value: bool) -> None:
         """Enable or disable OS-Agent diagnostics."""
         asyncio.create_task(
