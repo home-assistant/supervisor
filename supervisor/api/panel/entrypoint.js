5c66278a1cb5ecb8fdd1329cc93dd3489854c6ae
Joakim Sørensen <joasoe@gmail.com>
2021-10-07 11:37:16 +0200

    Bump frontend to 066a0771 (#3210)

--- supervisor/api/panel/entrypoint.js
+++ supervisor/api/panel/entrypoint.js
@@ -1,14 +1,14 @@
 
 function loadES5() {
   var el = document.createElement('script');
-  el.src = '/api/hassio/app/frontend_es5/entrypoint.beb8aae2.js';
+  el.src = '/api/hassio/app/frontend_es5/entrypoint.6ba12ae1.js';
   document.body.appendChild(el);
 }
 if (/.*Version\/(?:11|12)(?:\.\d+)*.*Safari\//.test(navigator.userAgent)) {
     loadES5();
 } else {
   try {
-    new Function("import('/api/hassio/app/frontend_latest/entrypoint.b658878e.js')")();
+    new Function("import('/api/hassio/app/frontend_latest/entrypoint.6a1c9a14.js')")();
   } catch (err) {
     loadES5();
   }
--- supervisor/api/panel/frontend_es5/9b327635.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkhome_assistant_frontend=self.webpackChunkhome_assistant_frontend||[]).push([[827],{64827:function(){Intl.PluralRules&&"function"==typeof Intl.PluralRules.__addLocaleData&&Intl.PluralRules.__addLocaleData({data:{categories:{cardinal:["one","other"],ordinal:["one","two","few","other"]},fn:function(e,n){var t=String(e).split("."),a=!t[1],l=Number(t[0])==e,o=l&&t[0].slice(-1),r=l&&t[0].slice(-2);return n?1==o&&11!=r?"one":2==o&&12!=r?"two":3==o&&13!=r?"few":"other":1==e&&a?"one":"other"}},locale:"en"})}}]);
-//# sourceMappingURL=9b327635.js.map
