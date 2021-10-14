beca09eeb775380502a2b85691fbdaefd1624754
Stefan Agner <stefan@agner.ch>
2021-10-14 15:41:40 +0200

    Replace signal impelmentation and cleanup

--- supervisor/utils/gdbus.py
+++ supervisor/utils/gdbus.py
@@ -1,18 +1,13 @@
 """DBus implementation with glib."""
 from __future__ import annotations
 
-import asyncio
-import json
 import logging
-import re
-import shlex
 from signal import SIGINT
 from typing import Any
 import xml.etree.ElementTree as ET
 
 import dbussy
 from dbussy import DBUS
-import sentry_sdk
 
 from . import clean_env
 from ..exceptions import (
@@ -25,49 +20,6 @@ from ..exceptions import (
 )
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
-
-# Use to convert GVariant into json
-RE_GVARIANT_TYPE: re.Pattern[Any] = re.compile(
-    r"\"[^\"\\]*(?:\\.[^\"\\]*)*\"|(boolean|byte|int16|uint16|int32|uint32|handle|int64|uint64|double|"
-    r"string|objectpath|signature|@[asviumodfy\{\}\(\)]+) "
-)
-RE_GVARIANT_VARIANT: re.Pattern[Any] = re.compile(r"\"[^\"\\]*(?:\\.[^\"\\]*)*\"|(<|>)")
-RE_GVARIANT_STRING_ESC: re.Pattern[Any] = re.compile(
-    r"(?<=(?: |{|\[|\(|<))'[^']*?\"[^']*?'(?=(?:|]|}|,|\)|>))"
-)
-RE_GVARIANT_STRING: re.Pattern[Any] = re.compile(
-    r"(?<=(?: |{|\[|\(|<))'(.*?)'(?=(?:|]|}|,|\)|>))"
-)
-RE_GVARIANT_BINARY: re.Pattern[Any] = re.compile(
-    r"\"[^\"\\]*(?:\\.[^\"\\]*)*\"|\[byte (.*?)\]|\[(0x[0-9A-Za-z]{2}.*?)\]|<byte (.*?)>"
-)
-RE_GVARIANT_BINARY_STRING: re.Pattern[Any] = re.compile(
-    r"\"[^\"\\]*(?:\\.[^\"\\]*)*\"|<?b\'(.*?)\'>?"
-)
-RE_GVARIANT_TUPLE_O: re.Pattern[Any] = re.compile(r"\"[^\"\\]*(?:\\.[^\"\\]*)*\"|(\()")
-RE_GVARIANT_TUPLE_C: re.Pattern[Any] = re.compile(
-    r"\"[^\"\\]*(?:\\.[^\"\\]*)*\"|(,?\))"
-)
-
-RE_BIN_STRING_OCT: re.Pattern[Any] = re.compile(r"\\\\(\d{3})")
-RE_BIN_STRING_HEX: re.Pattern[Any] = re.compile(r"\\\\x([0-9A-Za-z]{2})")
-
-RE_MONITOR_OUTPUT: re.Pattern[Any] = re.compile(
-    r".+?: (?P<signal>[^\s].+?) (?P<data>.*)"
-)
-
-# Map GDBus to errors
-MAP_GDBUS_ERROR: dict[str, Any] = {
-    "GDBus.Error:org.freedesktop.DBus.Error.ServiceUnknown": DBusInterfaceError,
-    "GDBus.Error:org.freedesktop.DBus.Error.Spawn.ChildExited": DBusFatalError,
-    "No such file or directory": DBusNotConnectedError,
-}
-
-# Commands for dbus
-INTROSPECT: str = "gdbus introspect --system --dest {bus} --object-path {object} --xml"
-CALL: str = "gdbus call --system --dest {bus} --object-path {object} --timeout 10 --method {method} {args}"
-MONITOR: str = "gdbus monitor --system --dest {bus}"
-WAIT: str = "gdbus wait --system --activate {bus} --timeout 5 {bus}"
 
 DBUS_METHOD_GETALL: str = "org.freedesktop.DBus.Properties.GetAll"
 DBUS_METHOD_SET: str = "org.freedesktop.DBus.Properties.Set"
@@ -87,19 +39,6 @@ def _remove_dbus_signature(data: Any) -> Any:
         return new_list
     else:
         return data
-
-
-def _convert_bytes(value: str) -> str:
-    """Convert bytes to string or byte-array."""
-    data: bytes = bytes(int(char, 0) for char in value.split(", "))
-    return f"[{', '.join(str(char) for char in data)}]"
-
-
-def _convert_bytes_string(value: str) -> str:
-    """Convert bytes to string or byte-array."""
-    data = RE_BIN_STRING_OCT.sub(lambda x: chr(int(x.group(1), 8)), value)
-    data = RE_BIN_STRING_HEX.sub(lambda x: chr(int(f"0x{x.group(1)}", 0)), data)
-    return f"[{', '.join(str(char) for char in list(char for char in data.encode()))}]"
 
 
 class DBus:
@@ -125,16 +64,6 @@ class DBus:
 
     async def _init_proxy(self) -> None:
         # """Read interface data."""
-        # # Wait for dbus object to be available after restart
-        # command_wait = shlex.split(WAIT.format(bus=self.bus_name))
-        # await self._send(command_wait, silent=True)
-
-        # # Introspect object & Parse XML
-        # command_introspect = shlex.split(
-        #     INTROSPECT.format(bus=self.bus_name, object=self.object_path)
-        # )
-        # data = await self._send(command_introspect)
-
         self._conn = await dbussy.Connection.bus_get_async(
             DBUS.BUS_SYSTEM, private=False
         )
@@ -146,7 +75,7 @@ class DBus:
             method="Introspect",
         )
 
-        reply = await self._conn.send_await_reply(message, timeout=5000)
+        reply = await self._conn.send_await_reply(message, timeout=10000)
         data = reply.expect_return_objects("s")[0]
         try:
             xml = ET.fromstring(data)
@@ -168,90 +97,6 @@ class DBus:
             for signal in interface.findall("./signal"):
                 signal_name = signal.get("name")
                 self.signals.add(f"{interface_name}.{signal_name}")
-
-    @staticmethod
-    def parse_gvariant(raw: str) -> Any:
-        """Parse GVariant input to python."""
-        # Process first string
-        json_raw = RE_GVARIANT_STRING_ESC.sub(
-            lambda x: x.group(0).replace('"', '\\"'), raw
-        )
-        json_raw = RE_GVARIANT_STRING.sub(r'"\1"', json_raw)
-
-        # Handle Bytes
-        json_raw = RE_GVARIANT_BINARY.sub(
-            lambda x: x.group(0)
-            if not (x.group(1) or x.group(2) or x.group(3))
-            else _convert_bytes(x.group(1) or x.group(2) or x.group(3)),
-            json_raw,
-        )
-        json_raw = RE_GVARIANT_BINARY_STRING.sub(
-            lambda x: x.group(0)
-            if not x.group(1)
-            else _convert_bytes_string(x.group(1)),
-            json_raw,
-        )
-
-        # Remove complex type handling
-        json_raw: str = RE_GVARIANT_TYPE.sub(
-            lambda x: x.group(0) if not x.group(1) else "", json_raw
-        )
-        json_raw = RE_GVARIANT_VARIANT.sub(
-            lambda x: x.group(0) if not x.group(1) else "", json_raw
-        )
-        json_raw = RE_GVARIANT_TUPLE_O.sub(
-            lambda x: x.group(0) if not x.group(1) else "[", json_raw
-        )
-        json_raw = RE_GVARIANT_TUPLE_C.sub(
-            lambda x: x.group(0) if not x.group(1) else "]", json_raw
-        )
-
-        # No data
-        if json_raw.startswith("[]"):
-            return []
-
-        try:
-            return json.loads(json_raw)
-        except json.JSONDecodeError as err:
-            _LOGGER.error("Can't parse '%s': '%s' - %s", json_raw, raw, err)
-            sentry_sdk.capture_exception(err)
-            raise DBusParseError() from err
-
-    @staticmethod
-    def gvariant_args(args: list[Any]) -> str:
-        """Convert args into gvariant."""
-        gvariant = ""
-        for arg in args:
-            if isinstance(arg, bool):
-                gvariant += f" {str(arg).lower()}"
-            elif isinstance(arg, (int, float)):
-                gvariant += f" {arg}"
-            elif isinstance(arg, str):
-                gvariant += f' "{arg}"'
-            else:
-                gvariant += f" {arg!s}"
-
-        return gvariant.lstrip()
-
-    async def call_dbus_old(self, method: str, *args: list[Any]) -> str:
-        """Call a dbus method."""
-        command = shlex.split(
-            CALL.format(
-                bus=self.bus_name,
-                object=self.object_path,
-                method=method,
-                args=self.gvariant_args(args),
-            )
-        )
-
-        # Run command
-        _LOGGER.debug("Call %s on %s", method, self.object_path)
-        data = await self._send(command)
-
-        # Parse and return data
-        parsed_data = self.parse_gvariant(data)
-        _LOGGER.debug(parsed_data)
-        return parsed_data
 
     async def call_dbus(self, method: str, *args: list[Any]) -> str:
         """Call a dbus method."""
@@ -278,12 +123,13 @@ class DBus:
             elif isinstance(arg, str):
                 request.append_objects("s", arg)
             else:
-                raise DBusFatalError(f"Type %s not supported")
+                _LOGGER.warning("No explicit support for type %s, assuming string", type(arg))
+                request.append_objects("s", str(arg))
 
         reply = await self._conn.send_await_reply(request)
 
         reply_object = _remove_dbus_signature(reply.all_objects)
-        _LOGGER.debug(reply_object)
+        #_LOGGER.debug(reply_object)
         return reply_object
 
     async def get_properties(self, interface: str) -> dict[str, Any]:
@@ -311,53 +157,37 @@ class DBus:
             _LOGGER.error("No Set attribute %s for %s", name, interface)
             raise DBusFatalError() from err
 
-    async def _send(self, command: list[str], silent=False) -> str:
-        """Send command over dbus."""
-        # Run command
-        _LOGGER.debug("Send D-Bus command: %s", command)
-        try:
-            proc = await asyncio.create_subprocess_exec(
-                *command,
-                stdin=asyncio.subprocess.DEVNULL,
-                stdout=asyncio.subprocess.PIPE,
-                stderr=asyncio.subprocess.PIPE,
-                env=clean_env(),
-            )
-
-            data, error = await proc.communicate()
-        except OSError as err:
-            _LOGGER.critical("D-Bus fatal error: %s", err)
-            raise DBusFatalError() from err
-
-        # Success?
-        if proc.returncode == 0 or silent:
-            return data.decode()
-
-        # Filter error
-        error = error.decode()
-        for msg, exception in MAP_GDBUS_ERROR.items():
-            if msg not in error:
-                continue
-            raise exception()
-
-        # General
-        _LOGGER.debug("D-Bus return: %s", error.strip())
-        raise DBusProgramError(error.strip())
-
-    def attach_signals(self, filters=None):
-        """Generate a signals wrapper."""
-        return DBusSignalWrapper(self, filters)
-
     async def wait_signal(self, signal):
         """Wait for single event."""
-        monitor = DBusSignalWrapper(self, [signal])
-        async with monitor as signals:
-            async for signal in signals:
-                return signal
+        _LOGGER.debug("wait_signal")
+        conn = await dbussy.Connection.bus_get_async(DBUS.BUS_SYSTEM, private=True)
+        conn.enable_receive_message({DBUS.MESSAGE_TYPE_SIGNAL})
+
+        signal_parts = signal.split(".")
+        interface = ".".join(signal_parts[:-1])
+        member = signal_parts[-1]
+
+        _LOGGER.debug("Use Bus AddMatch for interface %s, member %s", interface, member)
+        conn.enable_receive_message({DBUS.MESSAGE_TYPE_SIGNAL})
+        await conn.bus_add_match_async(f"type=signal,interface={interface},member={member}")
+
+        try:
+            while True:
+                message = await conn.receive_message_async()
+                _LOGGER.debug(
+                    "Message received: %s.%s: %s",
+                    message.interface,
+                    message.member,
+                    str(message.objects),   
+                )
+                if message.interface == interface and message.member == member:
+                    return _remove_dbus_signature(message.objects)
+        finally:
+            conn.close()
 
     def __getattr__(self, name: str) -> DBusCallWrapper:
         """Map to dbus method."""
-        return getattr(DBusCallWrapper(self, self.bus_name), name)
+        return getattr(DBusCallWrapper(self, self.bus_name), name)  
 
 
 class DBusCallWrapper:
@@ -388,70 +218,3 @@ class DBusCallWrapper:
             return self.dbus.call_dbus(interface, *args)
 
         return _method_wrapper
-
-
-class DBusSignalWrapper:
-    """Process Signals."""
-
-    def __init__(self, dbus: DBus, signals: str | None = None):
-        """Initialize dbus signal wrapper."""
-        self.dbus: DBus = dbus
-        self._signals: str | None = signals
-        self._proc: asyncio.Process | None = None
-
-    async def __aenter__(self):
-        """Start monitor events."""
-        _LOGGER.info("Starting dbus monitor on %s", self.dbus.bus_name)
-        command = shlex.split(MONITOR.format(bus=self.dbus.bus_name))
-        self._proc = await asyncio.create_subprocess_exec(
-            *command,
-            stdin=asyncio.subprocess.DEVNULL,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-            env=clean_env(),
-        )
-
-        return self
-
-    async def __aexit__(self, exception_type, exception_value, traceback):
-        """Stop monitor events."""
-        _LOGGER.info("Stopping dbus monitor on %s", self.dbus.bus_name)
-        self._proc.send_signal(SIGINT)
-        await self._proc.communicate()
-
-    def __aiter__(self):
-        """Start Iteratation."""
-        return self
-
-    async def __anext__(self):
-        """Get next data."""
-        if not self._proc:
-            raise StopAsyncIteration() from None
-
-        # Read signals
-        while True:
-            try:
-                data = await self._proc.stdout.readline()
-            except asyncio.TimeoutError:
-                raise StopAsyncIteration() from None
-
-            # Program close
-            if not data:
-                raise StopAsyncIteration() from None
-
-            # Extract metadata
-            match = RE_MONITOR_OUTPUT.match(data.decode())
-            if not match:
-                continue
-            signal = match.group("signal")
-            data = match.group("data")
-
-            # Filter signals?
-            if self._signals and signal not in self._signals:
-                _LOGGER.debug("Skiping event %s - %s", signal, data)
-                continue
-
-            try:
-                return self.dbus.parse_gvariant(data)
-            except DBusParseError as err:
-                raise StopAsyncIteration() from err
