da3d5b58933f1f98d3cd1cc1256a99fbb02a222c
Stefan Agner <stefan@agner.ch>
2021-10-15 18:08:15 +0200

    Remove template based network update code

--- supervisor/dbus/payloads/generate.py
+++ supervisor/dbus/payloads/generate.py
@@ -1,25 +1,15 @@
 """Payload generators for DBUS communication."""
 from __future__ import annotations
 
-from ipaddress import IPv4Address, IPv6Address, ip_address
-from pathlib import Path
-from re import split
 import socket
 from typing import TYPE_CHECKING, Any
 from uuid import uuid4
 from dbus_next.signature import Variant
 
-import jinja2
-
 from ...host.const import InterfaceType
 
 if TYPE_CHECKING:
     from ...host.network import Interface
-
-
-INTERFACE_UPDATE_TEMPLATE: Path = (
-    Path(__file__).parents[2].joinpath("dbus/payloads/interface_update.tmpl")
-)
 
 def get_connection_from_interface(interface: Interface, name: str | None = None, uuid: str | None = None
 ) -> Any:
@@ -129,40 +119,3 @@ def get_connection_from_interface(interface: Interface, name: str | None = None,
             conn["802-11-wireless-security"] = wireless_security
 
     return conn
-
-def interface_update_payload(
-    interface: Interface, name: str | None = None, uuid: str | None = None
-) -> str:
-    """Generate a payload for network interface update."""
-    env = jinja2.Environment()
-
-    def ipv4_to_int(ip_address: IPv4Address) -> int:
-        """Convert an ipv4 to an int."""
-        return socket.htonl(int(ip_address))
-
-    def ipv6_to_byte(ip_address: IPv6Address) -> str:
-        """Convert an ipv6 to an byte array."""
-        return f'[byte {", ".join(f"0x{val:02x}" for val in ip_address.packed)}]'
-
-    # Init template
-    env.filters["ipv4_to_int"] = ipv4_to_int
-    env.filters["ipv6_to_byte"] = ipv6_to_byte
-    template: jinja2.Template = env.from_string(INTERFACE_UPDATE_TEMPLATE.read_text())
-
-    # Generate UUID
-    if not uuid:
-        uuid = str(uuid4())
-
-    # Generate/Update ID/name
-    if not name or not name.startswith("Supervisor"):
-        name = f"Supervisor {interface.name}"
-    if interface.type == InterfaceType.VLAN:
-        name = f"{name}.{interface.vlan.id}"
-
-    # Fix SSID
-    if interface.wifi:
-        interface.wifi.ssid = ", ".join(
-            [f"0x{x}" for x in interface.wifi.ssid.encode().hex(",").split(",")]
-        )
-
-    return template.render(interface=interface, name=name, uuid=uuid)
