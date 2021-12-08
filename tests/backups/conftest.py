1bca1064e581e65e3e3efbf9af9b47a34efce874
Stefan Agner <stefan@agner.ch>
2021-12-08 20:17:18 +0000

    fix pylint warnings

--- tests/backups/conftest.py
+++ tests/backups/conftest.py
@@ -10,6 +10,22 @@ from supervisor.backups.validate import ALL_FOLDERS
 from tests.const import TEST_ADDON_SLUG
 
 
+@pytest.fixture(name="backup_mock")
+def fixture_backup_mock():
+    """Backup class mock."""
+    with patch("supervisor.backups.manager.Backup") as backup_mock:
+        backup_instance = MagicMock()
+        backup_mock.return_value = backup_instance
+
+        backup_instance.store_addons = AsyncMock(return_value=None)
+        backup_instance.store_folders = AsyncMock(return_value=None)
+        backup_instance.restore_addons = AsyncMock(return_value=None)
+        backup_instance.restore_folders = AsyncMock(return_value=None)
+        backup_instance.restore_repositories = AsyncMock(return_value=None)
+
+        yield backup_mock
+
+
 @pytest.fixture
 def partial_backup_mock(backup_mock):
     """Partial backup mock."""
@@ -28,19 +44,3 @@ def full_backup_mock(backup_mock):
     backup_instance.folders = ALL_FOLDERS
     backup_instance.addon_list = [TEST_ADDON_SLUG]
     yield backup_mock
-
-
-@pytest.fixture
-def backup_mock():
-    """Backup class mock."""
-    with patch("supervisor.backups.manager.Backup") as backup_mock:
-        backup_instance = MagicMock()
-        backup_mock.return_value = backup_instance
-
-        backup_instance.store_addons = AsyncMock(return_value=None)
-        backup_instance.store_folders = AsyncMock(return_value=None)
-        backup_instance.restore_addons = AsyncMock(return_value=None)
-        backup_instance.restore_folders = AsyncMock(return_value=None)
-        backup_instance.restore_repositories = AsyncMock(return_value=None)
-
-        yield backup_mock
