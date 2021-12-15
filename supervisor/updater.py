e8c065724c401537fb07fed65dd4cf43e44db727
Stefan Agner <stefan@agner.ch>
2021-12-15 10:32:08 +0000

    Log if board reported by HAOS isn't in version file
    Explicitly check if the board reported by HAOS is in the version file.
    Also make sure that we still set the download URL so it will still be
    possible to update to old versions in case needed.

--- supervisor/updater.py
+++ supervisor/updater.py
@@ -242,11 +242,17 @@ class Updater(FileConfiguration, CoreSysAttributes):
 
             # Update HassOS version
             if self.sys_os.board:
-                events.append("os")
-                self._data[ATTR_HASSOS] = AwesomeVersion(
-                    data["hassos"][self.sys_os.board]
-                )
                 self._data[ATTR_OTA] = data["ota"]
+                if self.sys_os.board in data["hassos"]:
+                    events.append("os")
+                    self._data[ATTR_HASSOS] = AwesomeVersion(
+                        data["hassos"][self.sys_os.board]
+                    )
+                else:
+                    _LOGGER.warning(
+                        "Board '%s' not found in version file. No OS updates.",
+                        self.sys_os.board,
+                    )
 
             # Update Home Assistant plugins
             self._data[ATTR_CLI] = AwesomeVersion(data["cli"])
