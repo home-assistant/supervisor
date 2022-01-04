0e892524014c0b5e4b7922e58ddb3d2d4504a7bb
Allen Porter <allen.porter@gmail.com>
2022-01-04 15:14:29 -0800

    Fix copy/paste errors in tests

--- tests/discovery/test_adguard.py
+++ tests/discovery/test_adguard.py
@@ -7,13 +7,13 @@ from supervisor.discovery.validate import valid_discovery_config
 
 
 def test_good_config():
-    """Test good deconz config."""
+    """Test good adguard config."""
 
     valid_discovery_config("adguard", {"host": "test", "port": 3812})
 
 
 def test_bad_config():
-    """Test good adguard config."""
+    """Test bad adguard config."""
 
     with pytest.raises(vol.Invalid):
         valid_discovery_config("adguard", {"host": "test"})
