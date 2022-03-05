b42dd7c294b0fe1b32777e02b5393dbadb4f7725
Mike Degatano <michael.degatano@gmail.com>
2022-03-05 13:56:37 -0500

    Added properties for dbus resolved

--- supervisor/dbus/resolved.py
+++ supervisor/dbus/resolved.py
@@ -1,10 +1,43 @@
 """D-Bus interface for systemd-resolved."""
+from __future__ import annotations
+
 import logging
+from typing import Any
 
 from ..exceptions import DBusError, DBusInterfaceError
 from ..utils.dbus import DBus
-from .const import DBUS_NAME_RESOLVED, DBUS_OBJECT_RESOLVED
-from .interface import DBusInterface
+from .const import (
+    DBUS_ATTR_CACHE_STATISTICS,
+    DBUS_ATTR_CURRENT_DNS_SERVER,
+    DBUS_ATTR_CURRENT_DNS_SERVER_EX,
+    DBUS_ATTR_DNS,
+    DBUS_ATTR_DNS_EX,
+    DBUS_ATTR_DNS_OVER_TLS,
+    DBUS_ATTR_DNS_STUB_LISTENER,
+    DBUS_ATTR_DNSSEC,
+    DBUS_ATTR_DNSSEC_NEGATIVE_TRUST_ANCHORS,
+    DBUS_ATTR_DNSSEC_STATISTICS,
+    DBUS_ATTR_DNSSEC_SUPPORTED,
+    DBUS_ATTR_DOMAINS,
+    DBUS_ATTR_FALLBACK_DNS,
+    DBUS_ATTR_FALLBACK_DNS_EX,
+    DBUS_ATTR_LLMNR,
+    DBUS_ATTR_LLMNR_HOSTNAME,
+    DBUS_ATTR_MULTICAST_DNS,
+    DBUS_ATTR_RESOLV_CONF_MODE,
+    DBUS_ATTR_TRANSACTION_STATISTICS,
+    DBUS_IFACE_RESOLVED_MANAGER,
+    DBUS_NAME_RESOLVED,
+    DBUS_OBJECT_RESOLVED,
+    DNSAddressFamily,
+    DNSOverTLSEnabled,
+    DNSSECValidation,
+    DNSStubListenerEnabled,
+    MulticastProtocolEnabled,
+    ResolvConfMode,
+)
+from .interface import DBusInterface, dbus_property
+from .utils import dbus_connected
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
 
@@ -13,6 +46,10 @@ class Resolved(DBusInterface):
     """Handle D-Bus interface for systemd-resolved."""
 
     name = DBUS_NAME_RESOLVED
+
+    def __init__(self):
+        """Initialize Properties."""
+        self.properties: dict[str, Any] = {}
 
     async def connect(self):
         """Connect to D-Bus."""
@@ -24,3 +61,128 @@ class Resolved(DBusInterface):
             _LOGGER.warning(
                 "Host has no systemd-resolved support. DNS will not work correctly."
             )
+
+    @property
+    @dbus_property
+    def cache_statistics(self) -> tuple[int, int, int] | None:
+        """Return current cache entries and hits and misses since last reset."""
+        return self.properties[DBUS_ATTR_CACHE_STATISTICS]
+
+    @property
+    @dbus_property
+    def current_dns_server(
+        self,
+    ) -> list[tuple[int, DNSAddressFamily, bytes]] | None:
+        """Return current DNS server."""
+        return self.properties[DBUS_ATTR_CURRENT_DNS_SERVER]
+
+    @property
+    @dbus_property
+    def current_dns_server_ex(
+        self,
+    ) -> list[tuple[int, DNSAddressFamily, bytes, int, str]] | None:
+        """Return current DNS server including port and server name."""
+        return self.properties[DBUS_ATTR_CURRENT_DNS_SERVER_EX]
+
+    @property
+    @dbus_property
+    def dns(self) -> list[tuple[int, DNSAddressFamily, bytes]] | None:
+        """Return DNS servers in use."""
+        return self.properties[DBUS_ATTR_DNS]
+
+    @property
+    @dbus_property
+    def dns_ex(self) -> list[tuple[int, DNSAddressFamily, bytes, int, str]] | None:
+        """Return DNS servers in use including port and server name."""
+        return self.properties[DBUS_ATTR_DNS_EX]
+
+    @property
+    @dbus_property
+    def dns_over_tls(self) -> DNSOverTLSEnabled | None:
+        """Return DNS over TLS enabled."""
+        return self.properties[DBUS_ATTR_DNS_OVER_TLS]
+
+    @property
+    @dbus_property
+    def dns_stub_listener(self) -> DNSStubListenerEnabled | None:
+        """Return DNS stub listener enabled on port 53."""
+        return self.properties[DBUS_ATTR_DNS_STUB_LISTENER]
+
+    @property
+    @dbus_property
+    def dnssec(self) -> DNSSECValidation | None:
+        """Return DNSSEC validation enforced."""
+        return self.properties[DBUS_ATTR_DNSSEC]
+
+    @property
+    @dbus_property
+    def dnssec_negative_trust_anchors(self) -> list[str] | None:
+        """Return DNSSEC negative trust anchors."""
+        return self.properties[DBUS_ATTR_DNSSEC_NEGATIVE_TRUST_ANCHORS]
+
+    @property
+    @dbus_property
+    def dnssec_statistics(self) -> tuple[int, int, int, int] | None:
+        """Return Secure, insecure, bogus, and indeterminate DNSSEC validations since last reset."""
+        return self.properties[DBUS_ATTR_DNSSEC_STATISTICS]
+
+    @property
+    @dbus_property
+    def dnssec_supported(self) -> bool | None:
+        """Return DNSSEC enabled and selected DNS servers support it."""
+        return self.properties[DBUS_ATTR_DNSSEC_SUPPORTED]
+
+    @property
+    @dbus_property
+    def domains(self) -> list[tuple[int, str, bool]] | None:
+        """Return search and routing domains in use."""
+        return self.properties[DBUS_ATTR_DOMAINS]
+
+    @property
+    @dbus_property
+    def fallback_dns(self) -> list[tuple[int, DNSAddressFamily, bytes]] | None:
+        """Return fallback DNS servers."""
+        return self.properties[DBUS_ATTR_FALLBACK_DNS]
+
+    @property
+    @dbus_property
+    def fallback_dns_ex(
+        self,
+    ) -> list[tuple[int, DNSAddressFamily, bytes, int, str]] | None:
+        """Return fallback DNS servers including port and server name."""
+        return self.properties[DBUS_ATTR_FALLBACK_DNS_EX]
+
+    @property
+    @dbus_property
+    def llmnr(self) -> MulticastProtocolEnabled | None:
+        """Return LLMNR enabled."""
+        return self.properties[DBUS_ATTR_LLMNR]
+
+    @property
+    @dbus_property
+    def llmnr_hostname(self) -> str | None:
+        """Return LLMNR hostname on network."""
+        return self.properties[DBUS_ATTR_LLMNR_HOSTNAME]
+
+    @property
+    @dbus_property
+    def multicast_dns(self) -> MulticastProtocolEnabled | None:
+        """Return MDNS enabled."""
+        return self.properties[DBUS_ATTR_MULTICAST_DNS]
+
+    @property
+    @dbus_property
+    def resolv_conf_mode(self) -> ResolvConfMode | None:
+        """Return how /etc/resolv.conf managed on host."""
+        return self.properties[DBUS_ATTR_RESOLV_CONF_MODE]
+
+    @property
+    @dbus_property
+    def transaction_statistics(self) -> tuple[int, int] | None:
+        """Return transactions processing and processed since last reset."""
+        return self.properties[DBUS_ATTR_TRANSACTION_STATISTICS]
+
+    @dbus_connected
+    async def update(self):
+        """Update Properties."""
+        self.properties = await self.dbus.get_properties(DBUS_IFACE_RESOLVED_MANAGER)
---
+++ tests/dbus/test_resolved.py
@@ -0,0 +1,115 @@
+"""Test systemd-resolved dbus interface."""
+
+from socket import AF_INET6, inet_aton, inet_pton
+from unittest.mock import patch
+
+import pytest
+
+from supervisor.coresys import CoreSys
+from supervisor.dbus.const import (
+    DNSOverTLSEnabled,
+    DNSSECValidation,
+    DNSStubListenerEnabled,
+    MulticastProtocolEnabled,
+    ResolvConfMode,
+)
+
+DNS_IP_FIELDS = [
+    "DNS",
+    "DNSEx",
+    "FallbackDNS",
+    "FallbackDNSEx",
+    "CurrentDNSServer",
+    "CurrentDNSServerEx",
+]
+
+
+@pytest.fixture(name="coresys_ip_bytes")
+async def fixture_coresys_ip_bytes(coresys: CoreSys) -> CoreSys:
+    """Coresys with ip addresses correctly mocked as bytes."""
+    get_properties = coresys.dbus.network.dbus.get_properties
+
+    async def mock_get_properties(dbus_obj, interface):
+        reply = await get_properties(interface)
+
+        for field in DNS_IP_FIELDS:
+            if field in reply and len(reply[field]) > 0:
+                if isinstance(reply[field][0], list):
+                    for entry in reply[field]:
+                        entry[2] = bytes(entry[2])
+                else:
+                    reply[field][2] = bytes(reply[field][2])
+
+        return reply
+
+    with patch("supervisor.utils.dbus.DBus.get_properties", new=mock_get_properties):
+        yield coresys
+
+
+async def test_dbus_resolved_info(coresys_ip_bytes: CoreSys):
+    """Test systemd-resolved dbus connection."""
+    coresys = coresys_ip_bytes
+
+    assert coresys.dbus.resolved.dns is None
+
+    await coresys.dbus.resolved.connect()
+    await coresys.dbus.resolved.update()
+
+    assert coresys.dbus.resolved.llmnr_hostname == "homeassistant"
+    assert coresys.dbus.resolved.llmnr == MulticastProtocolEnabled.YES
+    assert coresys.dbus.resolved.multicast_dns == MulticastProtocolEnabled.RESOLVE
+    assert coresys.dbus.resolved.dns_over_tls == DNSOverTLSEnabled.NO
+
+    assert len(coresys.dbus.resolved.dns) == 2
+    assert coresys.dbus.resolved.dns[0] == [0, 2, inet_aton("127.0.0.1")]
+    assert coresys.dbus.resolved.dns[1] == [0, 10, inet_pton(AF_INET6, "::1")]
+    assert len(coresys.dbus.resolved.dns_ex) == 2
+    assert coresys.dbus.resolved.dns_ex[0] == [0, 2, inet_aton("127.0.0.1"), 0, ""]
+    assert coresys.dbus.resolved.dns_ex[1] == [0, 10, inet_pton(AF_INET6, "::1"), 0, ""]
+
+    assert len(coresys.dbus.resolved.fallback_dns) == 2
+    assert coresys.dbus.resolved.fallback_dns[0] == [0, 2, inet_aton("1.1.1.1")]
+    assert coresys.dbus.resolved.fallback_dns[1] == [
+        0,
+        10,
+        inet_pton(AF_INET6, "2606:4700:4700::1111"),
+    ]
+    assert len(coresys.dbus.resolved.fallback_dns_ex) == 2
+    assert coresys.dbus.resolved.fallback_dns_ex[0] == [
+        0,
+        2,
+        inet_aton("1.1.1.1"),
+        0,
+        "cloudflare-dns.com",
+    ]
+    assert coresys.dbus.resolved.fallback_dns_ex[1] == [
+        0,
+        10,
+        inet_pton(AF_INET6, "2606:4700:4700::1111"),
+        0,
+        "cloudflare-dns.com",
+    ]
+
+    assert coresys.dbus.resolved.current_dns_server == [0, 2, inet_aton("127.0.0.1")]
+    assert coresys.dbus.resolved.current_dns_server_ex == [
+        0,
+        2,
+        inet_aton("127.0.0.1"),
+        0,
+        "",
+    ]
+
+    assert len(coresys.dbus.resolved.domains) == 1
+    assert coresys.dbus.resolved.domains[0] == [0, "local.hass.io", False]
+
+    assert coresys.dbus.resolved.transaction_statistics == [0, 100000]
+    assert coresys.dbus.resolved.cache_statistics == [10, 50000, 10000]
+    assert coresys.dbus.resolved.dnssec == DNSSECValidation.NO
+    assert coresys.dbus.resolved.dnssec_statistics == [0, 0, 0, 0]
+    assert coresys.dbus.resolved.dnssec_supported is False
+    assert coresys.dbus.resolved.dnssec_negative_trust_anchors == [
+        "168.192.in-addr.arpa",
+        "local",
+    ]
+    assert coresys.dbus.resolved.dns_stub_listener == DNSStubListenerEnabled.NO
+    assert coresys.dbus.resolved.resolv_conf_mode == ResolvConfMode.FOREIGN
