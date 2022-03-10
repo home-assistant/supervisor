27a01c4ca42d27a7477fb9b0e9eceb7c4f23c78d
Pascal Vizeli <pvizeli@syshack.ch>
2022-03-10 12:51:03 +0000

    Revert exception that are not forwarded

--- supervisor/resolution/checks/core_trust.py
+++ supervisor/resolution/checks/core_trust.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 from ...const import CoreState
 from ...coresys import CoreSys
-from ...exceptions import CodeNotaryBackendError, CodeNotaryError, CodeNotaryUntrusted
+from ...exceptions import CodeNotaryError, CodeNotaryUntrusted
 from ..const import ContextType, IssueType, UnhealthyReason
 from .base import CheckBase
 
@@ -32,8 +32,6 @@ class CheckCoreTrust(CheckBase):
         except CodeNotaryUntrusted:
             self.sys_resolution.unhealthy = UnhealthyReason.UNTRUSTED
             self.sys_resolution.create_issue(IssueType.TRUST, ContextType.CORE)
-        except CodeNotaryBackendError:
-            _LOGGER.warning("CAS backend issue, skipping check")
         except CodeNotaryError:
             pass
 
