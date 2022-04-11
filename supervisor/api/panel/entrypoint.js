a8bafd680ec348d52a1c72b65e295dea42deba18
Ludeeus <ludeeus@ludeeus.dev>
2022-04-11 13:23:50 +0000

    Bump frontend to d900e40d

--- supervisor/api/panel/entrypoint.js
+++ supervisor/api/panel/entrypoint.js
@@ -1,14 +1,14 @@
 
 function loadES5() {
   var el = document.createElement('script');
-  el.src = '/api/hassio/app/frontend_es5/entrypoint.e6c1e3c6.js';
+  el.src = '/api/hassio/app/frontend_es5/entrypoint.c3a8263d.js';
   document.body.appendChild(el);
 }
 if (/.*Version\/(?:11|12)(?:\.\d+)*.*Safari\//.test(navigator.userAgent)) {
     loadES5();
 } else {
   try {
-    new Function("import('/api/hassio/app/frontend_latest/entrypoint.17311809.js')")();
+    new Function("import('/api/hassio/app/frontend_latest/entrypoint.531ccfea.js')")();
   } catch (err) {
     loadES5();
   }
