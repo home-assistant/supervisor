0564772036b31c081d4ab5f7a82cd2581eac6f03
Ludeeus <ludeeus@ludeeus.dev>
2022-05-03 17:16:36 +0000

    Bump frontend to 080cad0c

--- supervisor/api/panel/entrypoint.js
+++ supervisor/api/panel/entrypoint.js
@@ -1,14 +1,14 @@
 
 function loadES5() {
   var el = document.createElement('script');
-  el.src = '/api/hassio/app/frontend_es5/entrypoint.ed292e94.js';
+  el.src = '/api/hassio/app/frontend_es5/entrypoint.71729952.js';
   document.body.appendChild(el);
 }
 if (/.*Version\/(?:11|12)(?:\.\d+)*.*Safari\//.test(navigator.userAgent)) {
     loadES5();
 } else {
   try {
-    new Function("import('/api/hassio/app/frontend_latest/entrypoint.af342c20.js')")();
+    new Function("import('/api/hassio/app/frontend_latest/entrypoint.5bfacc21.js')")();
   } catch (err) {
     loadES5();
   }
