c3f7a45d61e418b18668f1ac95676a21fc340c69
Mike Degatano <michael.degatano@gmail.com>
2022-08-17 16:50:08 -0400

    Fix memory calculation for cgroupv2 (#3802)

--- tests/docker/test_stats.py
+++ tests/docker/test_stats.py
@@ -1,20 +1,40 @@
 """Test docker stats."""
+import pytest
+
 from supervisor.docker.stats import DockerStats
 
 from tests.common import load_json_fixture
 
 
-def test_cpu_presentage(docker):
-    """Test CPU presentage."""
-    stats_fixtrue = load_json_fixture("container_stats.json")
-    stats = DockerStats(stats_fixtrue)
+@pytest.mark.parametrize(
+    "stats_mock",
+    ["container_stats", "container_stats_cgroupv1", "container_stats_cgroupv2"],
+)
+def test_cpu_percentage(stats_mock: str):
+    """Test CPU percentage."""
+    stats_fixture = load_json_fixture(f"{stats_mock}.json")
+    stats = DockerStats(stats_fixture)
 
-    stats._calc_cpu_percent(stats_fixtrue)  # pylint: disable=protected-access
+    stats._calc_cpu_percent(stats_fixture)  # pylint: disable=protected-access
     assert stats.cpu_percent == 90.0
 
-    stats_fixtrue["cpu_stats"]["cpu_usage"]["total_usage"] = 0
-    stats_fixtrue["precpu_stats"]["cpu_usage"]["total_usage"] = 0
-    stats_fixtrue["cpu_stats"]["system_cpu_usage"] = 0
-    stats_fixtrue["precpu_stats"]["system_cpu_usage"] = 0
-    stats._calc_cpu_percent(stats_fixtrue)  # pylint: disable=protected-access
+    stats_fixture["cpu_stats"]["cpu_usage"]["total_usage"] = 0
+    stats_fixture["precpu_stats"]["cpu_usage"]["total_usage"] = 0
+    stats_fixture["cpu_stats"]["system_cpu_usage"] = 0
+    stats_fixture["precpu_stats"]["system_cpu_usage"] = 0
+    stats._calc_cpu_percent(stats_fixture)  # pylint: disable=protected-access
     assert stats.cpu_percent == 0.0
+
+
+@pytest.mark.parametrize(
+    "stats_mock",
+    ["container_stats", "container_stats_cgroupv1", "container_stats_cgroupv2"],
+)
+async def test_memory_usage(stats_mock: str):
+    """Test memory usage calculation."""
+    stats_fixture = load_json_fixture(f"{stats_mock}.json")
+    stats = DockerStats(stats_fixture)
+
+    assert stats.memory_limit == 4000000000
+    assert stats.memory_usage == 59700000
+    assert stats.memory_percent == 1.49
