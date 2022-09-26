4b12a1e63abe7353f7176584bb75893507679657
Mike Degatano <michael.degatano@gmail.com>
2022-09-26 19:56:19 -0400

    pylint fix

--- supervisor/dbus/utils.py
+++ supervisor/dbus/utils.py
@@ -9,7 +9,7 @@ def dbus_connected(method):
     def wrap_dbus(api, *args, **kwargs):
         """Check if D-Bus is connected before call a method."""
         if api.is_shutdown:
-            return
+            return None
         if api.dbus is None:
             raise DBusNotConnectedError()
         return method(api, *args, **kwargs)
