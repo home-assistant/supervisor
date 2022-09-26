d388860e455e48dc83bb24aafe8dab8f24d73cc8
Mike Degatano <michael.degatano@gmail.com>
2022-09-26 11:53:33 -0400

    Bad introspection causes DBus error not KeyError

---
+++ tests/utils/test_dbus.py
@@ -0,0 +1,16 @@
+"""Test dbus utility."""
+from dbus_fast.aio.message_bus import MessageBus
+import pytest
+
+from supervisor.dbus.const import DBUS_OBJECT_BASE
+from supervisor.exceptions import DBusInterfaceMethodError
+from supervisor.utils.dbus import DBus
+
+
+async def test_missing_properties_interface(dbus_bus: MessageBus, dbus: list[str]):
+    """Test introspection missing properties interface."""
+    service = await DBus.connect(
+        dbus_bus, "test.no.properties.interface", DBUS_OBJECT_BASE
+    )
+    with pytest.raises(DBusInterfaceMethodError):
+        await service.get_properties("test.no.properties.interface")
