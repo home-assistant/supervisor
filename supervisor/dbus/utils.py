4db31cd59a97fc235d121c0197ceea91ea8444b3
Mike Degatano <michael.degatano@gmail.com>
2022-09-26 20:24:19 -0400

    noop coroutine and test

--- supervisor/dbus/utils.py
+++ supervisor/dbus/utils.py
@@ -3,13 +3,17 @@
 from ..exceptions import DBusNotConnectedError
 
 
+async def noop():
+    """Return a noop coroutine."""
+
+
 def dbus_connected(method):
     """Wrap check if D-Bus is connected."""
 
     def wrap_dbus(api, *args, **kwargs):
         """Check if D-Bus is connected before call a method."""
         if api.is_shutdown:
-            return None
+            return noop()
         if api.dbus is None:
             raise DBusNotConnectedError()
         return method(api, *args, **kwargs)
