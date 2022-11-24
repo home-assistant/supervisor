2d907b0e160538611b4e892e4d2742fbf66e1ef4
Pascal Vizeli <pvizeli@syshack.ch>
2022-11-24 11:29:57 +0000

    add tests

--- tests/addons/test_build.py
+++ tests/addons/test_build.py
@@ -51,3 +51,25 @@ async def test_dockerfile_evaluation_arch(coresys: CoreSys, install_addon_ssh: A
         "fixtures/addons/local/ssh/Dockerfile.aarch64"
     )
     assert build.arch == "aarch64"
+
+
+async def test_build_valid(coresys: CoreSys, install_addon_ssh: Addon):
+    """Test platform set in docker args."""
+    build = AddonBuild(coresys, install_addon_ssh)
+    with patch.object(
+        type(coresys.arch), "supported", new=PropertyMock(return_value=["aarch64"])
+    ), patch.object(
+        type(coresys.arch), "default", new=PropertyMock(return_value="aarch64")
+    ):
+        assert build.is_valid
+
+
+async def test_build_invalid(coresys: CoreSys, install_addon_ssh: Addon):
+    """Test platform set in docker args."""
+    build = AddonBuild(coresys, install_addon_ssh)
+    with patch.object(
+        type(coresys.arch), "supported", new=PropertyMock(return_value=["amd64"])
+    ), patch.object(
+        type(coresys.arch), "default", new=PropertyMock(return_value="amd64")
+    ):
+        assert not build.is_valid
