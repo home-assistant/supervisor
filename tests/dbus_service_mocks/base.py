31fe8258ab0a95249ecccd1c5739a4d9279fb563
Mike Degatano <michael.degatano@gmail.com>
2023-03-20 23:18:00 -0400

    func instead of fn for pylint

--- tests/dbus_service_mocks/base.py
+++ tests/dbus_service_mocks/base.py
@@ -19,13 +19,13 @@ def dbus_method(name: str = None, disabled: bool = False):
     orig_decorator = method(name=name, disabled=disabled)
 
     @no_type_check_decorator
-    def decorator(fn):
+    def decorator(func):
         calls: list[list[Any]] = []
 
-        @wraps(fn)
+        @wraps(func)
         def track_calls(self, *args):
             calls.append(args)
-            return fn(self, *args)
+            return func(self, *args)
 
         wrapped = orig_decorator(track_calls)
         wrapped.__dict__["calls"] = calls
