4cc6458dac6c15a3904cc4701d07006968235705
ludeeus <joasoe@gmail.com>
2023-04-14 11:52:37 +0000

    Bump frontend to 3a1fff81

--- supervisor/api/panel/entrypoint.js
+++ supervisor/api/panel/entrypoint.js
@@ -1,14 +1,14 @@
 
 function loadES5() {
   var el = document.createElement('script');
-  el.src = '/api/hassio/app/frontend_es5/entrypoint.c28a3c3a.js';
+  el.src = '/api/hassio/app/frontend_es5/entrypoint--6PDbD45dS8.js';
   document.body.appendChild(el);
 }
 if (/.*Version\/(?:11|12)(?:\.\d+)*.*Safari\//.test(navigator.userAgent)) {
     loadES5();
 } else {
   try {
-    new Function("import('/api/hassio/app/frontend_latest/entrypoint.fc042133.js')")();
+    new Function("import('/api/hassio/app/frontend_latest/entrypoint-Rzm-3XAKFKI.js')")();
   } catch (err) {
     loadES5();
   }
