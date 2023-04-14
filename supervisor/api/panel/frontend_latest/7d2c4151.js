a54e0a8401870a0e20d41b26ea5ca2b528fcf96b
Joakim Sørensen <joasoe@gmail.com>
2023-04-14 15:07:42 +0200

    Bump frontend to 3a1fff81 (#4241)

--- supervisor/api/panel/frontend_latest/7d2c4151.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkhome_assistant_frontend=self.webpackChunkhome_assistant_frontend||[]).push([[2811],{32811:(s,e,n)=>{n.r(e),n.d(e,{default:()=>a});var t=n(56087);t.ZP.mount(t.V8,new t.lK);const a=t.ZP}}]);
