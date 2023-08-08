71077fb0f78e0c164185ff2ba6606317f10227d1
Mike Degatano <michael.degatano@gmail.com>
2023-08-07 20:53:25 -0400

    Fallback on interface name if path is missing (#4479)

--- tests/dbus/network/setting/test_generate.py
+++ tests/dbus/network/setting/test_generate.py
@@ -1,6 +1,9 @@
 """Test settings generation from interface."""
 
+from unittest.mock import PropertyMock, patch
+
 from supervisor.dbus.network import NetworkManager
+from supervisor.dbus.network.interface import NetworkInterface
 from supervisor.dbus.network.setting.generate import get_connection_from_interface
 from supervisor.host.network import Interface
 
@@ -24,3 +27,17 @@ async def test_get_connection_from_interface(network_manager: NetworkManager):
 
     assert connection_payload["ipv6"]["method"].value == "auto"
     assert "address-data" not in connection_payload["ipv6"]
+
+
+async def test_get_connection_no_path(network_manager: NetworkManager):
+    """Test network interface without a path."""
+    dbus_interface = network_manager.get(TEST_INTERFACE)
+    with patch.object(NetworkInterface, "path", new=PropertyMock(return_value=None)):
+        interface = Interface.from_dbus_interface(dbus_interface)
+
+    connection_payload = get_connection_from_interface(interface)
+
+    assert "connection" in connection_payload
+    assert "match" not in connection_payload
+
+    assert connection_payload["connection"]["interface-name"].value == "eth0"
