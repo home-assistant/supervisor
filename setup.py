1c815dcad113bdf13aba49d7ef4c02bbed67409a
Chris Carini <6374067+ChrisCarini@users.noreply.github.com>
2023-10-03 13:22:12 -0700

    Remove all PyPi classifiers as this package is not published to PyPi (#4574)
    * Update PyPi classifier to Python 3.11
    
    * remove all classifiers
    
    * Update setup.py
    
    Co-authored-by: Mike Degatano <michael.degatano@gmail.com>
    
    ---------
    
    Co-authored-by: Mike Degatano <michael.degatano@gmail.com>

--- setup.py
+++ setup.py
@@ -15,18 +15,6 @@ setup(
         "A maintainless private cloud operator system that"
         "setup a Home-Assistant instance. Based on HassOS"
     ),
-    classifiers=[
-        "Intended Audience :: End Users/Desktop",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Topic :: Home Automation",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Scientific/Engineering :: Atmospheric Science",
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3.8",
-    ],
     keywords=["docker", "home-assistant", "api"],
     zip_safe=False,
     platforms="any",
