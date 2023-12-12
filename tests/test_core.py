5c3bd9d483f9829cea7b6aef646ba31197c2be0f
Stefan Agner <stefan@agner.ch>
2023-12-12 19:10:10 +0000

    Add test when system time is behind by more than 3 days

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,11 +1,13 @@
 """Testing handling with CoreState."""
 # pylint: disable=W0212
 import datetime
-from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock, PropertyMock, patch
 
 from supervisor.const import CoreState
 from supervisor.coresys import CoreSys
 from supervisor.exceptions import WhoamiSSLError
+from supervisor.host.control import SystemControl
+from supervisor.host.info import InfoCenter
 from supervisor.utils.whoami import WhoamiData
 
 
@@ -52,3 +54,22 @@ async def test_adjust_system_datetime_without_ssl(run_dir, coresys: CoreSys):
         assert mock_retrieve_whoami.call_args_list[0].args[1]
         assert not mock_retrieve_whoami.call_args_list[1].args[1]
         assert coresys.core.sys_config.timezone == "Europe/Zurich"
+
+
+async def test_adjust_system_datetime_if_time_behind(run_dir, coresys: CoreSys):
+    """Test _adjust_system_datetime method when current time is ahead more than 3 days."""
+    utc_ts = datetime.datetime.now().replace(tzinfo=datetime.UTC) + datetime.timedelta(
+        days=4
+    )
+    with patch(
+        "supervisor.core.retrieve_whoami",
+        new_callable=AsyncMock,
+        side_effect=[WhoamiData("Europe/Zurich", utc_ts)],
+    ) as mock_retrieve_whoami, patch.object(
+        SystemControl, "set_datetime"
+    ) as mock_set_datetime, patch.object(
+        InfoCenter, "dt_synchronized", new=PropertyMock(return_value=False)
+    ):
+        await coresys.core._adjust_system_datetime()
+        mock_retrieve_whoami.assert_called_once()
+        mock_set_datetime.assert_called_once()
