00626af541952266e75fea3e298dc609c7357787
Mike Degatano <michael.degatano@gmail.com>
2023-12-19 15:23:40 -0500

    Share requirements and fix version import

--- setup.py
+++ setup.py
@@ -1,6 +1,27 @@
 """Home Assistant Supervisor setup."""
+from pathlib import Path
+import re
+
 from setuptools import setup
 
-from supervisor.const import SUPERVISOR_VERSION
+RE_SUPERVISOR_VERSION = re.compile(r"^SUPERVISOR_VERSION =\s*(.+)$")
 
-setup(version=SUPERVISOR_VERSION)
+SUPERVISOR_DIR = Path(__file__).parent
+REQUIREMENTS_FILE = SUPERVISOR_DIR / "requirements.txt"
+CONST_FILE = SUPERVISOR_DIR / "supervisor/const.py"
+
+REQUIREMENTS = REQUIREMENTS_FILE.read_text(encoding="utf-8")
+CONSTANTS = CONST_FILE.read_text(encoding="utf-8")
+
+
+def _get_supervisor_version():
+    for line in CONSTANTS.split("/n"):
+        if match := RE_SUPERVISOR_VERSION.match(line):
+            return match.group(1)
+    return "99.9.9dev"
+
+
+setup(
+    version=_get_supervisor_version(),
+    dependencies=REQUIREMENTS.split("/n"),
+)
