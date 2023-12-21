a32e5a398291e302edfd24d78b382866ad2ccf2e
Stefan Agner <stefan@agner.ch>
2023-12-21 09:39:22 +0000

    Fix pytest

--- tests/addons/test_manager.py
+++ tests/addons/test_manager.py
@@ -68,7 +68,7 @@ async def test_image_added_removed_on_update(
         await coresys.addons.update(TEST_ADDON_SLUG)
         build.assert_not_called()
         install.assert_called_once_with(
-            AwesomeVersion("10.0.0"), "test/amd64-my-ssh-addon", False, None
+            AwesomeVersion("10.0.0"), "test/amd64-my-ssh-addon", False, "amd64"
         )
 
     assert install_addon_ssh.need_update is False
