dcd670b9767594766b97567e00ba9467733757d9
J. Nick Koston <nick@koston.org>
2024-01-12 09:21:01 -1000

    port relevant tests

--- tests/utils/test_json.py
+++ tests/utils/test_json.py
@@ -1,5 +1,11 @@
 """test json."""
-from supervisor.utils.json import write_json_file
+import json
+import time
+from typing import NamedTuple
+
+import pytest
+
+from supervisor.utils.json import json_dumps, json_loads, write_json_file
 
 
 def test_file_permissions(tmp_path):
@@ -18,3 +24,50 @@ def test_new_file_permissions(tmp_path):
 
     write_json_file(tempfile, {"test": "data"})
     assert oct(tempfile.stat().st_mode)[-3:] == "600"
+
+
+async def test_loading_derived_class():
+    """Test loading data from classes derived from str."""
+
+    class MyStr(str):
+        pass
+
+    class MyBytes(bytes):
+        pass
+
+    assert json_loads('"abc"') == "abc"
+    assert json_loads(MyStr('"abc"')) == "abc"
+
+    assert json_loads(b'"abc"') == "abc"
+    with pytest.raises(json.JSONDecodeError):
+        assert json_loads(MyBytes(b'"abc"')) == "abc"
+
+
+def test_json_dumps_float_subclass() -> None:
+    """Test the json dumps a float subclass."""
+
+    class FloatSubclass(float):
+        """A float subclass."""
+
+    assert json_dumps({"c": FloatSubclass(1.2)}) == '{"c":1.2}'
+
+
+def test_json_dumps_tuple_subclass() -> None:
+    """Test the json dumps a tuple subclass."""
+
+    tt = time.struct_time((1999, 3, 17, 32, 44, 55, 2, 76, 0))
+
+    assert json_dumps(tt) == "[1999,3,17,32,44,55,2,76,0]"
+
+
+def test_json_dumps_named_tuple_subclass() -> None:
+    """Test the json dumps a tuple subclass."""
+
+    class NamedTupleSubclass(NamedTuple):
+        """A NamedTuple subclass."""
+
+        name: str
+
+    nts = NamedTupleSubclass("a")
+
+    assert json_dumps(nts) == '["a"]'
