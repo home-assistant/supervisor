34ecd4d539aa6a0be65401f62f37619f16f6693d
Mike Degatano <michael.degatano@gmail.com>
2025-01-16 15:47:17 +0000

    Move is_restore to backup manager

--- supervisor/backups/manager.py
+++ supervisor/backups/manager.py
@@ -47,6 +47,9 @@ from .validate import ALL_FOLDERS, SCHEMA_BACKUPS_CONFIG
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
 
+JOB_FULL_RESTORE = "backup_manager_full_restore"
+JOB_PARTIAL_RESTORE = "backup_manager_partial_restore"
+
 
 class BackupManager(FileConfiguration, JobGroup):
     """Manage backups."""
@@ -85,6 +88,16 @@ class BackupManager(FileConfiguration, JobGroup):
             for mount in self.sys_mounts.backup_mounts
             if mount.state == UnitActiveState.ACTIVE
         }
+
+    @property
+    def current_restore(self) -> str | None:
+        """Return id of current restore job if a restore job is in progress."""
+        job = self.sys_jobs.current
+        while job.parent_id:
+            job = self.sys_jobs.get_job(job.parent_id)
+            if job.name in {JOB_FULL_RESTORE, JOB_PARTIAL_RESTORE}:
+                return job.uuid
+        return None
 
     def get(self, slug: str) -> Backup:
         """Return backup object."""
@@ -657,7 +670,7 @@ class BackupManager(FileConfiguration, JobGroup):
                     )
 
     @Job(
-        name="backup_manager_full_restore",
+        name=JOB_FULL_RESTORE,
         conditions=[
             JobCondition.FREE_SPACE,
             JobCondition.HEALTHY,
@@ -721,7 +734,7 @@ class BackupManager(FileConfiguration, JobGroup):
         return success
 
     @Job(
-        name="backup_manager_partial_restore",
+        name=JOB_PARTIAL_RESTORE,
         conditions=[
             JobCondition.FREE_SPACE,
             JobCondition.HEALTHY,
