dfc17a980b0abf7a6ef83dcf2c10ab25caef2c17
Stefan Agner <stefan@agner.ch>
2025-01-31 15:55:06 +0100

    Update tests

--- tests/store/test_validate.py
+++ tests/store/test_validate.py
@@ -10,36 +10,8 @@ from supervisor.store.validate import SCHEMA_STORE_FILE, repositories
 
 
 @pytest.mark.parametrize(
-    "config",
-    [
-        {},
-        {ATTR_REPOSITORIES: []},
-        {ATTR_REPOSITORIES: ["https://github.com/esphome/home-assistant-addon"]},
-    ],
-)
-async def test_default_config(config: dict[Any]):
-    """Test built-ins included by default."""
-    conf = SCHEMA_STORE_FILE(config)
-    assert ATTR_REPOSITORIES in conf
-    assert "core" in conf[ATTR_REPOSITORIES]
-    assert "local" in conf[ATTR_REPOSITORIES]
-    assert "https://github.com/hassio-addons/repository" in conf[ATTR_REPOSITORIES]
-    assert (
-        len(
-            [
-                repo
-                for repo in conf[ATTR_REPOSITORIES]
-                if repo == "https://github.com/esphome/home-assistant-addon"
-            ]
-        )
-        == 1
-    )
-
-
-@pytest.mark.parametrize(
     "repo_list,valid",
     [
-        ([], True),
         (["core", "local"], True),
         (["https://github.com/hassio-addons/repository"], True),
         (["not_a_url"], False),
@@ -50,14 +22,8 @@ async def test_repository_validate(repo_list: list[str], valid: bool):
     """Test repository list validate."""
     if valid:
         processed = repositories(repo_list)
-        assert len(processed) == 5
-        assert set(repositories(repo_list)) == {
-            "core",
-            "local",
-            "https://github.com/hassio-addons/repository",
-            "https://github.com/esphome/home-assistant-addon",
-            "https://github.com/music-assistant/home-assistant-addon",
-        }
+        assert len(processed) == len(repo_list)
+        assert set(repositories(repo_list)) == set(repo_list)
     else:
         with pytest.raises(Invalid):
             repositories(repo_list)
