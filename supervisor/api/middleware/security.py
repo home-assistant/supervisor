home-assistant/supervisor

commit c9db42583b39f1bfe5d0cf46c7bb87c9c45f1f0b

Capture typeerror exception (#2052)

supervisor/utils/log_format.py
@@ -1,15 +1,22 @@
 """Custom log messages."""
+import logging
 import re
+
+import sentry_sdk
+
+_LOGGER: logging.Logger = logging.getLogger(__name__)
 
 RE_BIND_FAILED = re.compile(r".*Bind for.*:(\d*) failed: port is already allocated.*")
 
 
 def format_message(message: str) -> str:
     """Return a formated message if it's known."""
-    match = RE_BIND_FAILED.match(message)
-    if match:
-        return (
-            f"Port '{match.group(1)}' is already in use by something else on the host."
-        )
+    try:
+        match = RE_BIND_FAILED.match(message)
+        if match:
+            return f"Port '{match.group(1)}' is already in use by something else on the host."
+    except TypeError as err:
+        _LOGGER.error("Type of message is not string - %s", err)
+        sentry_sdk.capture_exception(err)
 
     return message

tests/utils/test_log_format.py
@@ -9,3 +9,9 @@ def test_format_message():
         format_message(message)
         == "Port '80' is already in use by something else on the host."
     )
+
+
+def test_exeption():
+    """Tests the exception handling."""
+    message = b"byte"
+    assert format_message(message) == message

commit 670bddd15d2250f57209acaec289aca30e26bcdd

Drop Sentry capture from format_message
The error handling got introduced in #2052, however, #2100 essentially
makes sure there will never be a byte object passed to this function.
And even if, the Sentry aiohttp plug-in will properly catch such an
exception.

supervisor/api/middleware/security.py
@@ -208,7 +208,7 @@ class SecurityMiddleware(CoreSysAttributes):
             CoreState.RUNNING,
             CoreState.FREEZE,
         ):
-            return await api_return_error(
+            return api_return_error(
                 message=f"System is not ready with state: {self.sys_core.state}"
             )
 

supervisor/api/utils.py
@@ -63,11 +63,11 @@ def api_process(method):
         try:
             answer = await method(api, *args, **kwargs)
         except BackupFileNotFoundError as err:
-            return await api_return_error(err, status=404)
+            return api_return_error(err, status=404)
         except APIError as err:
-            return await api_return_error(err, status=err.status, job_id=err.job_id)
+            return api_return_error(err, status=err.status, job_id=err.job_id)
         except HassioError as err:
-            return await api_return_error(err)
+            return api_return_error(err)
 
         if isinstance(answer, (dict, list)):
             return api_return_ok(data=answer)
@@ -76,7 +76,7 @@ def api_process(method):
         if isinstance(answer, web.StreamResponse):
             return answer
         elif isinstance(answer, bool) and not answer:
-            return await api_return_error()
+            return api_return_error()
         return api_return_ok()
 
     return wrap_api
@@ -107,14 +107,14 @@ def api_process_raw(content, *, error_type=None):
             try:
                 msg_data = await method(api, *args, **kwargs)
             except APIError as err:
-                return await api_return_error(
+                return api_return_error(
                     err,
                     error_type=error_type or const.CONTENT_TYPE_BINARY,
                     status=err.status,
                     job_id=err.job_id,
                 )
             except HassioError as err:
-                return await api_return_error(
+                return api_return_error(
                     err, error_type=error_type or const.CONTENT_TYPE_BINARY
                 )
 
@@ -128,7 +128,7 @@ def api_process_raw(content, *, error_type=None):
     return wrap_method
 
 
-async def api_return_error(
+def api_return_error(
     error: Exception | None = None,
     message: str | None = None,
     error_type: str | None = None,
@@ -139,7 +139,7 @@ async def api_return_error(
     if error and not message:
         message = get_message_from_exception_chain(error)
         if check_exception_chain(error, DockerAPIError):
-            message = await format_message(message)
+            message = format_message(message)
     if not message:
         message = "Unknown error, see supervisor"
 

supervisor/utils/log_format.py
@@ -3,8 +3,6 @@
 import logging
 import re
 
-from .sentry import async_capture_exception
-
 _LOGGER: logging.Logger = logging.getLogger(__name__)
 
 RE_BIND_FAILED = re.compile(
@@ -12,14 +10,12 @@ RE_BIND_FAILED = re.compile(
 )
 
 
-async def format_message(message: str) -> str:
+def format_message(message: str) -> str:
     """Return a formatted message if it's known."""
-    try:
-        match = RE_BIND_FAILED.match(message)
-        if match:
-            return f"Port '{match.group(1)}' is already in use by something else on the host."
-    except TypeError as err:
-        _LOGGER.error("The type of message is not a string - %s", err)
-        await async_capture_exception(err)
+    match = RE_BIND_FAILED.match(message)
+    if match:
+        return (
+            f"Port '{match.group(1)}' is already in use by something else on the host."
+        )
 
     return message

tests/utils/test_log_format.py
@@ -3,25 +3,19 @@
 from supervisor.utils.log_format import format_message
 
 
-async def test_format_message_port():
+def test_format_message_port():
     """Tests for message formater."""
     message = '500 Server Error: Internal Server Error:  Bind for 0.0.0.0:80 failed: port is already allocated")'
     assert (
-        await format_message(message)
+        format_message(message)
         == "Port '80' is already in use by something else on the host."
     )
 
 
-async def test_format_message_port_alternative():
+def test_format_message_port_alternative():
     """Tests for message formater."""
     message = 'Error starting userland proxy: listen tcp 0.0.0.0:80: bind: address already in use")'
     assert (
-        await format_message(message)
+        format_message(message)
         == "Port '80' is already in use by something else on the host."
     )
-
-
-async def test_exeption():
-    """Tests the exception handling."""
-    message = b"byte"
-    assert await format_message(message) == message