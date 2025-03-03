7584fd9f8d0067acac41b5ec0e1b8e0b3664aa9d
Mike Degatano <michael.degatano@gmail.com>
2025-03-03 23:53:46 +0000

    Fix format_message

--- tests/utils/test_log_format.py
+++ tests/utils/test_log_format.py
@@ -3,7 +3,7 @@
 from supervisor.utils.log_format import format_message
 
 
-async def test_format_message_port():
+def test_format_message_port():
     """Tests for message formater."""
     message = '500 Server Error: Internal Server Error:  Bind for 0.0.0.0:80 failed: port is already allocated")'
     assert (
@@ -12,7 +12,7 @@ async def test_format_message_port():
     )
 
 
-async def test_format_message_port_alternative():
+def test_format_message_port_alternative():
     """Tests for message formater."""
     message = 'Error starting userland proxy: listen tcp 0.0.0.0:80: bind: address already in use")'
     assert (
