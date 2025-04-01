5c1f7ed18da685da85006e9ae12e6b6753261dc8
Mike Degatano <michael.degatano@gmail.com>
2025-04-01 15:49:33 -0400

    Switch iter_chunked to iter_chunks (#5798)
    * Switch iter_chunked to iter_any
    
    * iter_chunks not iter_any

--- supervisor/api/ingress.py
+++ supervisor/api/ingress.py
@@ -279,7 +279,7 @@ class APIIngress(CoreSysAttributes):
             try:
                 response.headers["X-Accel-Buffering"] = "no"
                 await response.prepare(request)
-                async for data in result.content.iter_chunked(4096):
+                async for data, _ in result.content.iter_chunks():
                     await response.write(data)
 
             except (
