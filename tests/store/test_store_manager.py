1c4ec0d328a74d2b5a3ce753e7a133fa28721e0a
Stefan Agner <stefan@agner.ch>
2025-04-25 18:28:18 +0200

    Fix pytest failure
    Now that we actually only reload metadata if the local store changed
    we have to fake the change as well to fix the store manager tests.

--- tests/store/test_store_manager.py
+++ tests/store/test_store_manager.py
@@ -250,5 +250,8 @@ async def test_addon_version_timestamp(coresys: CoreSys, install_addon_example: 
 
     # If a new version is seen processing repo, reset to utc now
     install_addon_example.data_store["version"] = "1.1.0"
-    await coresys.store.reload()
-    assert timestamp < install_addon_example.latest_version_timestamp
+
+    # Signal the store repositories got updated
+    with patch("supervisor.store.repository.Repository.update", return_value=True):
+        await coresys.store.reload()
+        assert timestamp < install_addon_example.latest_version_timestamp
