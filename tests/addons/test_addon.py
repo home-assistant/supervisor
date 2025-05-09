39bd20c0e795a0ffbd2d076d1e8b4abed6cb47b2
Stefan Agner <stefan@agner.ch>
2025-05-09 11:07:22 +0200

    Handle non-existing addon config dir (#5871)
    * Handle non-existing addon config dir
    
    Since users have access to the root of all add-on config directories,
    they can delete the directory of an add-ons at any time. Hence we need
    to handle gracefully if it doesn't exist anymore.
    
    * Add pytest

--- tests/addons/test_addon.py
+++ tests/addons/test_addon.py
@@ -446,6 +446,27 @@ async def test_backup(
     assert await install_addon_ssh.backup(tarfile) is None
 
 
+@pytest.mark.parametrize("status", ["running", "stopped"])
+async def test_backup_no_config(
+    coresys: CoreSys,
+    install_addon_ssh: Addon,
+    container: MagicMock,
+    status: str,
+    tmp_supervisor_data,
+    path_extern,
+) -> None:
+    """Test backing up an addon with deleted config directory."""
+    container.status = status
+
+    install_addon_ssh.data["map"].append({"type": "addon_config", "read_only": False})
+    assert not install_addon_ssh.path_config.exists()
+    install_addon_ssh.path_data.mkdir()
+    await install_addon_ssh.load()
+
+    tarfile = SecureTarFile(coresys.config.path_tmp / "test.tar.gz", "w")
+    assert await install_addon_ssh.backup(tarfile) is None
+
+
 async def test_backup_with_pre_post_command(
     coresys: CoreSys,
     install_addon_ssh: Addon,
