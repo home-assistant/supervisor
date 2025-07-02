9f0759e05f94cfdb025b0a0f01cf0e7e2e9365fe
Felipe Santos <felipecassiors@gmail.com>
2025-07-02 12:12:43 -0300

    Use docker buildx build rather than docker build
    Co-authored-by: Stefan Agner <stefan@agner.ch>

--- supervisor/addons/build.py
+++ supervisor/addons/build.py
@@ -130,6 +130,7 @@ class AddonBuild(FileConfiguration, CoreSysAttributes):
 
         build_cmd = [
             "docker",
+            "buildx",
             "build",
             ".",
             "--tag",
