d2cc165387b0ddc378fbaec0c1aa09a361b2b69c
Stefan Agner <stefan@agner.ch>
2025-07-16 13:01:39 +0200

    Enable IPv6 by default for new installations
    Enable IPv6 by default for new Supervisor installations. Let's also
    make the `enable_ipv6` attribute nullable, so we can distinguish
    between "not set" and "set to false".

--- supervisor/api/docker.py
+++ supervisor/api/docker.py
@@ -32,7 +32,7 @@ SCHEMA_DOCKER_REGISTRY = vol.Schema(
 )
 
 # pylint: disable=no-value-for-parameter
-SCHEMA_OPTIONS = vol.Schema({vol.Optional(ATTR_ENABLE_IPV6): vol.Boolean()})
+SCHEMA_OPTIONS = vol.Schema({vol.Optional(ATTR_ENABLE_IPV6): vol.Maybe(vol.Boolean())})
 
 
 class APIDocker(CoreSysAttributes):
