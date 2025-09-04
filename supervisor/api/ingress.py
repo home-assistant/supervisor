a3a5f6ba98be96d1b7a9fd863a378f800ef9701f
Felipe Santos <felipecassiors@gmail.com>
2025-09-04 06:12:42 -0300

    Fix WebSocket proxy for add-ons not forwarding ping/pong frame data (#6144)
    * Fix proxied add-on WebSocket connections closing after 40 seconds
    
    * Undo autoping=True
    
    * Add debug logging for ping/pong frames
    
    * Foward ping and pong msg.data too
    
    * Add temporary workaround for devcontainer issue
    
    * Forward 8000 through docker devcontainer
    
    * Remove debug code

--- supervisor/api/ingress.py
+++ supervisor/api/ingress.py
@@ -391,9 +391,9 @@ async def _websocket_forward(ws_from, ws_to):
             elif msg.type == aiohttp.WSMsgType.BINARY:
                 await ws_to.send_bytes(msg.data)
             elif msg.type == aiohttp.WSMsgType.PING:
-                await ws_to.ping()
+                await ws_to.ping(msg.data)
             elif msg.type == aiohttp.WSMsgType.PONG:
-                await ws_to.pong()
+                await ws_to.pong(msg.data)
             elif ws_to.closed:
                 await ws_to.close(code=ws_to.close_code, message=msg.extra)
     except RuntimeError:
