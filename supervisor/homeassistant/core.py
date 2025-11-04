0e506efcfb22460206f28f57bc883144421c440e
Stefan Agner <stefan@agner.ch>
2025-11-04 17:50:08 +0100

    Exclude Core from architecture deprecation checks
    This allows to download the latest available Core version still, even
    on deprecated systems.

--- supervisor/homeassistant/core.py
+++ supervisor/homeassistant/core.py
@@ -222,7 +222,6 @@ class HomeAssistantCore(JobGroup):
             JobCondition.INTERNET_HOST,
             JobCondition.PLUGINS_UPDATED,
             JobCondition.SUPERVISOR_UPDATED,
-            JobCondition.ARCHITECTURE_SUPPORTED,
         ],
         on_condition=HomeAssistantJobError,
         concurrency=JobConcurrency.GROUP_REJECT,
