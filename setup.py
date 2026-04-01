fefc4e362181ed655425f32bf4ca7d30e03842be
Jan Čermák <sairon@sairon.cz>
2026-04-01 10:31:51 +0200

    Fix version/requirements parsing in setup.py, set version in Dockerfile
    Remove the step patching const.py with detected version in CI and do
    this during Dockerfile version from BUILD_VERSION argument instead.
    
    Also, fix parsing of the version in `setup.py` - it wasn't working
    because it was splitting the files on "/n" instead of newlines,
    resulting in all Python packages installed having version string set to
    `9999.9.9.dev9999` instead of the expected version.
    
    (Note: setuptools are doing a version string normalization, so the
    installed package has stripped leading zeroes and the second component
    is `9` instead of the literal `09` used in default string in all places.
    Fixing this is out of scope of this change as the ideal solution would
    be to change the versioning schema but it should be noted.)
    
    Lastly, clean up builder.yaml environment variables (crane is not used
    anymore after #6679).

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import re
 
 from setuptools import setup
 
-RE_SUPERVISOR_VERSION = re.compile(r"^SUPERVISOR_VERSION =\s*(.+)$")
+RE_SUPERVISOR_VERSION = re.compile(r'^SUPERVISOR_VERSION =\s*"?([^"]+)"?$')
 
 SUPERVISOR_DIR = Path(__file__).parent
 REQUIREMENTS_FILE = SUPERVISOR_DIR / "requirements.txt"
@@ -16,7 +16,7 @@ CONSTANTS = CONST_FILE.read_text(encoding="utf-8")
 
 
 def _get_supervisor_version():
-    for line in CONSTANTS.split("/n"):
+    for line in CONSTANTS.split("\n"):
         if match := RE_SUPERVISOR_VERSION.match(line):
             return match.group(1)
     return "9999.09.9.dev9999"
@@ -24,5 +24,5 @@ def _get_supervisor_version():
 
 setup(
     version=_get_supervisor_version(),
-    dependencies=REQUIREMENTS.split("/n"),
+    dependencies=REQUIREMENTS.split("\n"),
 )
