c77c39b038084062b8cd12d56116c6134afc5156
Franck Nijhof <git@frenck.dev>
2026-07-02 17:16:04 +0200

    Update supervisor/backups/manager.py
    Co-authored-by: Stefan Agner <stefan@agner.ch>

--- supervisor/backups/manager.py
+++ supervisor/backups/manager.py
@@ -969,7 +969,7 @@ class BackupManager(FileConfiguration, JobGroup):
         is_running: list[bool] = await asyncio.gather(
             *[app.is_running() for app in installed]
         )
-        running_apps = [app for app, running in zip(installed, is_running) if running]
+        running_apps = [app for app, running in zip(installed, is_running, strict=True) if running]
 
         # Create thaw task first to ensure we eventually undo freezes even if the below fails
         self._thaw_task = asyncio.shield(
